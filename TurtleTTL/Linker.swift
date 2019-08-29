1d7e08218efe2cef94d1ddc7cc03d14f240e68f6
foxostro <foxostro@gmail.com>
2019-08-29 11:00:49 -0700

    Add stub Linker class and tests

---
+++ TurtleTTL/Linker.swift
@@ -0,0 +1,38 @@
+//
+//  Linker.swift
+//  TurtleTTL
+//
+//  Created by Andrew Fox on 8/29/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class Linker: NSObject {
+//    let codeGenerator: CodeGenerator
+//
+//    // Maps from the symbol name to the symbol value.
+//    public private(set) var symbols: [String:Int] = [:]
+//
+//    // Maps from the index of the instruction to the name of the unresolved
+//    // symbol. The instruction must be rewritten to use the actual value for
+//    // this symbol.
+//    public private(set) var unresolvedSymbols: [Int:String] = [:]
+//
+//    public required init(codeGenerator: CodeGenerator) {
+//        self.codeGenerator = codeGenerator
+//    }
+//
+//    public func compile(_ root: AbstractSyntaxTreeNode) throws -> [Instruction] {
+//        codeGenerator.begin()
+//        try root.iterate {
+//            try $0.accept(visitor: self)
+//        }
+//        codeGenerator.end()
+//        return link(inputInstructions: codeGenerator.instructions)
+//    }
+//
+//    func link(inputInstructions: [Instruction]) {
+//
+//    }
+}

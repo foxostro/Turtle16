09f46672565a120899a840110862ac7a861c5070
foxostro <foxostro@gmail.com>
2019-08-29 11:00:49 -0700

    Add stub Linker class and tests

---
+++ TurtleTTLTests/LinkerTests.swift
@@ -0,0 +1,16 @@
+//
+//  LinkerTests.swift
+//  TurtleTTLTests
+//
+//  Created by Andrew Fox on 8/29/19.
+//  Copyright © 2019 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+import TurtleTTL
+
+class LinkerTests: XCTestCase {
+    func testInit() {
+        XCTFail()
+    }
+}

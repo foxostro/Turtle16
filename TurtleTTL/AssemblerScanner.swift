6347a244f10e07ee52d5d7b172cfe358be5a9c08
foxostro <foxostro@gmail.com>
2019-09-02 01:09:08 -0700

    AssemblerScanner contains _only_ the assembler-specific logic.

--- TurtleTTL/AssemblerScanner.swift
+++ TurtleTTL/AssemblerScanner.swift
@@ -9,113 +9,84 @@
 import Cocoa
 
 public class AssemblerScanner: TurtleScanner {
-    struct Rule {
-        let pattern: String
-        let emit: (AssemblerScanner, String) -> Token?
-    }
-    
-    let rules: [Rule] = [
-        Rule(pattern: "\n") {
-            let token = TokenNewline(lineNumber: $0.lineNumber, lexeme: $1)
-            $0.lineNumber += 1
-            return token
-        },
-        Rule(pattern: "((#)|(//))") {(scanner: AssemblerScanner, lexeme: String) in
-            scanner.advanceToNewline()
-            return nil
-        },
-        Rule(pattern: ",") {
-            TokenComma(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: ":") {
-            TokenColon(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "NOP\\b") {
-            TokenNOP(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "CMP\\b") {
-            TokenCMP(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "HLT\\b") {
-            TokenHLT(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "JMP\\b") {
-            TokenJMP(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "JC\\b") {
-            TokenJC(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "ADD\\b") {
-            TokenADD(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "LI\\b") {
-            TokenLI(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "MOV\\b") {
-            TokenMOV(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "STORE\\b") {
-            TokenSTORE(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "LOAD\\b") {
-            TokenLOAD(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "[ABCDEMXY]\\b") {
-            TokenRegister(lineNumber: $0.lineNumber, lexeme: $1, literal: $1)
-        },
-        Rule(pattern: "[_a-zA-Z][_a-zA-Z0-9]+\\b") {
-            TokenIdentifier(lineNumber: $0.lineNumber, lexeme: $1)
-        },
-        Rule(pattern: "[-]{0,1}[0-9]+\\b") {
-            let scanner = Scanner(string: $1)
-            var number: Int = 0
-            var result = scanner.scanInt(&number)
-            assert(result)
-            return TokenNumber(lineNumber: $0.lineNumber, lexeme: $1, literal: number)
-        },
-        Rule(pattern: "\\$[0-9a-fA-F]+\\b") {
-            let scanner = Scanner(string: String($1.dropFirst()))
-            var number: UInt32 = 0
-            var result = scanner.scanHexInt32(&number)
-            assert(result)
-            return TokenNumber(lineNumber: $0.lineNumber, lexeme: $1, literal: Int(number))
-        },
-        Rule(pattern: "0[xX][0-9a-fA-F]+\\b") {
-            let scanner = Scanner(string: String($1))
-            var number: UInt32 = 0
-            var result = scanner.scanHexInt32(&number)
-            assert(result)
-            return TokenNumber(lineNumber: $0.lineNumber, lexeme: $1, literal: Int(number))
-        },
-        Rule(pattern: "[ \t]+") {(scanner: AssemblerScanner, lexeme: String) in
-            nil
-        }
-    ]
-    
-    public private(set) var tokens: [Token] = []
-    var lineNumber = 1
-    
-    public func scanTokens() throws {
-        while !isAtEnd {
-            try scanToken()
-        }
-        tokens.append(TokenEOF(lineNumber: lineNumber, lexeme: ""))
-    }
-    
-    public func scanToken() throws {
-        for rule in rules {
-            if let lexeme = match(pattern: rule.pattern) {
-                if let token = rule.emit(self, lexeme) {
-                    tokens.append(token)
-                }
-                return
+    public required init(withString string: String) {
+        super.init(withString: string)
+        self.rules = [
+            Rule(pattern: "\n") {
+                let token = TokenNewline(lineNumber: self.lineNumber, lexeme: $0)
+                self.lineNumber += 1
+                return token
+            },
+            Rule(pattern: "((#)|(//))") {(lexeme: String) in
+                self.advanceToNewline()
+                return nil
+            },
+            Rule(pattern: ",") {
+                TokenComma(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: ":") {
+                TokenColon(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "NOP\\b") {
+                TokenNOP(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "CMP\\b") {
+                TokenCMP(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "HLT\\b") {
+                TokenHLT(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "JMP\\b") {
+                TokenJMP(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "JC\\b") {
+                TokenJC(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "ADD\\b") {
+                TokenADD(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "LI\\b") {
+                TokenLI(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "MOV\\b") {
+                TokenMOV(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "STORE\\b") {
+                TokenSTORE(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "LOAD\\b") {
+                TokenLOAD(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "[ABCDEMXY]\\b") {
+                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: $0)
+            },
+            Rule(pattern: "[_a-zA-Z][_a-zA-Z0-9]+\\b") {
+                TokenIdentifier(lineNumber: self.lineNumber, lexeme: $0)
+            },
+            Rule(pattern: "[-]{0,1}[0-9]+\\b") {
+                let scanner = Scanner(string: $0)
+                var number: Int = 0
+                let result = scanner.scanInt(&number)
+                assert(result)
+                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: number)
+            },
+            Rule(pattern: "\\$[0-9a-fA-F]+\\b") {
+                let scanner = Scanner(string: String($0.dropFirst()))
+                var number: UInt32 = 0
+                let result = scanner.scanHexInt32(&number)
+                assert(result)
+                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: Int(number))
+            },
+            Rule(pattern: "0[xX][0-9a-fA-F]+\\b") {
+                let scanner = Scanner(string: String($0))
+                var number: UInt32 = 0
+                let result = scanner.scanHexInt32(&number)
+                assert(result)
+                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: Int(number))
+            },
+            Rule(pattern: "[ \t]+") {(lexeme: String) in
+                nil
             }
-        }
-        
-        throw unexpectedCharacterError(peek()!)
-    }
-    
-    func unexpectedCharacterError(_ character: String) -> AssemblerError {
-        return AssemblerError(line: lineNumber, format: "unexpected character: `%@'", character)
+        ]
     }
 }

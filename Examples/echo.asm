ac22bdc2f4fcf2cf06c6d93371ff0d4f999bf7ef
foxostro <foxostro@gmail.com>
2019-11-08 21:02:55 -0800

    Arduino uses a bit of shared memory to communicate with the CPU.
    This represents a hardware change to the serial card which has not been
    entered into the schematic yet.
    
    BUG: If the CPU is running slowly enough then the Arduino may mistake a
    single slow write for multiple commands.

--- Examples/echo.asm
+++ Examples/echo.asm
@@ -1,52 +1,47 @@
 # let kSerialInterface = 6
 # let kIORegister = 0
 # let kStatusRegister = 1
+# let kCommandPort = 0
+# let kDataPort = 1
+# let kReady = 0
+# let kCommandRead = 1
+# let kCommandWrite = 2
+# let kCommandAvail = 3
+# let kCommandInit = 4
 
 LI A, 0
 LI B, 0
-LI X, 0
-LI Y, 0
 LI U, 0
 LI V, 0
-
+LI X, 0
 LI D, 6 # kSerialInterface
-LI P, 'r'
-LI P, 'e'
-LI P, 'a'
-LI P, 'd'
-LI P, 'y'
-LI P, '.'
-LI P, 10
 
-
-# Now, we enter a loop where we wait for serial input and then echo it to
-# serial output.
-
-beginningOfInputLoop:
-
-waitForInput:
-LI Y, 1 # kStatusRegister
+# Initialize the serial interface device.
+LI Y, 0 # kCommandPort
+LI P, 4 # kCommandInit
+LI B, 0 # kReady
+LXY wait_for_serial_init
+wait_for_serial_init:
 MOV A, P
-LI B, 0
 CMP
-LXY waitForInput
-JE
+NOP
+JNE
 NOP
 NOP
 
-# Read a byte and echo it back.
-LI Y, 0 # kIORegister
+# Send a command to write a character.
+LI Y, 1 # kDataPort
+LI P, '!'
+LI Y, 0 # kCommandPort
+LI P, 2 # kCommandWrite
+LI B, 0 # kReady
+LXY wait_for_character_write
+wait_for_character_write:
 MOV A, P
-MOV P, A
+CMP
+NOP
+JNE
+NOP
+NOP
 
-LXY beginningOfInputLoop
-JMP
-NOP
-NOP
-LI A, 0xff
-LI B, 0xff
-LI U, 0xff
-LI V, 0xff
-LI X, 0xff
-LI Y, 0xff
 HLT

f92132d85a5596b5d9d22e8318cb81fac8a71db7
foxostro <foxostro@gmail.com>
2019-11-22 00:40:30 -0800

    Updated Arduino sketch and example program for another change to Serial.
    The Arduino serial interface module now operates like an SPI slave, except
    that it is eight bits wide. Implemented a protocol where the Arduino can
    respond to commands and return a status code.
    
    Updated the example app so that I can use it debug this new functionality.

--- Examples/arduino_serial_module_2.asm
+++ Examples/arduino_serial_module_2.asm
@@ -2,10 +2,45 @@ LI A, 0
 LI B, 0
 LI D, 6 # Serial Module
 LI X, 0 # X0 is SCK
-LI Y, 1 # Y0 is MOSI
+LI Y, 0 # Y[0..7] is MOSI
+
+# Reset Serial Interface
+LI A, 0
+LI Y, 0 # doCommandReset
 LI X, 1
 NOP
-MOV P, A
+MOV A, P # Store the status in A
 LI X, 0
-LI Y, 0
+
+# Put an '@'
+LI A, 0
+LI Y, 1 # kCommandPutByte
+LI X, 1
+NOP
+MOV A, P # Store the status in A
+LI X, 0
+
+LI A, 0
+LI Y, '@'
+LI X, 1
+NOP
+MOV A, P # Store the status in A
+LI X, 0
+
+# Get the number of bytes available
+LI A, 0
+LI Y, 3 # kCommandGetByte
+LI X, 1
+NOP
+MOV B, P
+LI X, 0
+
+# Get a byte
+LI A, 0
+LI Y, 2 # kCommandGetByte
+LI X, 1
+NOP
+MOV A, P
+LI X, 0
+
 HLT

88e19f5972b7d09089dd6d88028619ab6ffaef22
foxostro <foxostro@gmail.com>
2019-11-24 13:06:33 -0800

    Update serial test program for another change to hardeware.
    Added a register to the serial module which is latched when inputting a
    value to the serial register. This replaces the X0 bit as SCK.

--- Examples/arduino_serial_module_2.asm
+++ Examples/arduino_serial_module_2.asm
@@ -1,46 +1,45 @@
 LI A, 0
 LI B, 0
 LI D, 6 # Serial Module
-LI X, 0 # X0 is SCK
 LI Y, 0 # Y[0..7] is MOSI
 
 # Reset Serial Interface
 LI A, 0
 LI Y, 0 # doCommandReset
-LI X, 1
+LI P, 1 # Raise SCK
 NOP
 MOV A, P # Store the status in A
-LI X, 0
+LI P, 0 # Lower SCK
 
 # Put an '@'
 LI A, 0
 LI Y, 1 # kCommandPutByte
-LI X, 1
+LI P, 1 # Raise SCK
 NOP
 MOV A, P # Store the status in A
-LI X, 0
+LI P, 0 # Lower SCK
 
 LI A, 0
 LI Y, '@'
-LI X, 1
+LI P, 1 # Raise SCK
 NOP
 MOV A, P # Store the status in A
-LI X, 0
+LI P, 0 # Lower SCK
 
 # Get the number of bytes available
 LI A, 0
 LI Y, 3 # kCommandGetByte
-LI X, 1
+LI P, 1 # Raise SCK
 NOP
 MOV B, P
-LI X, 0
+LI P, 0 # Lower SCK
 
 # Get a byte
 LI A, 0
 LI Y, 2 # kCommandGetByte
-LI X, 1
+LI P, 1 # Raise SCK
 NOP
 MOV A, P
-LI X, 0
+LI P, 0 # Lower SCK
 
 HLT

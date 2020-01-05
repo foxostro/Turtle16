312119f6f1f310f35e2cdc1ba82b58493b93fd5a
foxostro <foxostro@gmail.com>
2020-01-05 14:09:44 -0800

    doLoad() and doStore() should be in a test-specific class extension.

--- TurtleTTLTests/SimulatorTests/SerialInterfacePeripheralTests.swift
+++ TurtleTTLTests/SimulatorTests/SerialInterfacePeripheralTests.swift
@@ -9,6 +9,31 @@
 import XCTest
 import TurtleTTL
 
+extension SerialInterfacePeripheral {
+    func doLoad(at address: UInt16) {
+        setAddress(address)
+        bus = Register(withValue: 0)
+        PI = .inactive
+        PO = .active
+        onControlClock()
+        onRegisterClock()
+    }
+    
+    func doStore(value: UInt8, at address: UInt16) {
+        setAddress(address)
+        bus = Register(withValue: value)
+        PI = .active
+        PO = .inactive
+        onControlClock()
+        onRegisterClock()
+    }
+    
+    func setAddress(_ address: UInt16) {
+        registerX = Register(withValue: UInt8((address & 0xff) >> 8))
+        registerY = Register(withValue: UInt8(address & 0xff))
+    }
+}
+
 class SerialInterfacePeripheralTests: XCTestCase {
     func testInitiallyEmptySerialInputAndOutput() {
         let serial = SerialInterfacePeripheral()
@@ -32,53 +57,31 @@ class SerialInterfacePeripheralTests: XCTestCase {
     func testReadFromRegisterOneToGetCount() {
         let serial = SerialInterfacePeripheral()
         serial.serialInput = makeStringBytes("hello")
-        doLoad(serial, at: 1)
+        serial.doLoad(at: 1)
         XCTAssertEqual(serial.bus.value, 5)
     }
     
     func testReadFromRegisterZeroToGetNextCharacter() {
         let serial = SerialInterfacePeripheral()
         serial.serialInput = makeStringBytes("hello")
-        doLoad(serial, at: 0)
+        serial.doLoad(at: 0)
         XCTAssertEqual(serial.bus.value, makeStringBytes("h").first!)
     }
     
     func testReadFromRegisterZeroYields255WhenNoInput() {
         let serial = SerialInterfacePeripheral()
         serial.serialInput = makeStringBytes("")
-        doLoad(serial, at: 0)
+        serial.doLoad(at: 0)
         XCTAssertEqual(serial.bus.value, 255)
     }
     
     func testWriteToRegisterZeroToOutputCharacter() {
         let serial = SerialInterfacePeripheral()
-        doStore(serial, value: 65, at: 0)
+        serial.doStore(value: 65, at: 0)
         XCTAssertEqual(serial.describeSerialOutput(), "A")
     }
     
     func makeStringBytes(_ string: String) -> [UInt8] {
         return Array(string.data(using: .utf8)!)
-    }
-    
-    func doLoad(_ serial: SerialInterfacePeripheral, at address: UInt16) {
-        serial.registerX = Register(withValue: UInt8((address & 0xff) >> 8))
-        serial.registerY = Register(withValue: UInt8(address & 0xff))
-        serial.bus = Register(withValue: 0)
-        serial.PI = .inactive
-        serial.PO = .active
-        serial.onControlClock()
-        serial.onRegisterClock()
-    }
-    
-    func doStore(_ serial: SerialInterfacePeripheral,
-                 value: UInt8,
-                 at address: UInt16) {
-        serial.registerX = Register(withValue: UInt8((address & 0xff) >> 8))
-        serial.registerY = Register(withValue: UInt8(address & 0xff))
-        serial.bus = Register(withValue: value)
-        serial.PI = .active
-        serial.PO = .inactive
-        serial.onControlClock()
-        serial.onRegisterClock()
     }
 }

c641f9a8f75b044fb8bce2ba9540b0a7d7f15e32
foxostro <foxostro@gmail.com>
2020-01-05 17:08:23 -0800

    Update the Simulator to simulate the new Serial Interface hardware in Rev 3.

--- TurtleTTL/Simulator/SerialInterfacePeripheral.swift
+++ TurtleTTL/Simulator/SerialInterfacePeripheral.swift
@@ -9,14 +9,40 @@
 import Cocoa
 
 public class SerialInterfacePeripheral: ComputerPeripheral {
+    public let kDataRegister: UInt16 = 1
+    public let kControlRegister: UInt16 = 0
+    
+    public let kStatusSuccess: UInt8 = 0xff
+    
+    public let kCommandResetSerialLink: UInt8 = 0
+    public let kCommandPutByte: UInt8 = 1
+    public let kCommandGetByte: UInt8 = 2
+    public let kCommandGetNumBytes: UInt8 = 3
+    
+    var microState: UInt8
+    let kMicroStateWaitingForRisingSCK: UInt8 = 0
+    let kMicroStateWaitingForFallingSCK: UInt8 = 1
+    let kMicroStateProcessing: UInt8 = 2
+    
+    var state: UInt8
+    let kStateIdle: UInt8 = 0
+    let kStateWaitingForOutputByte: UInt8 = 1
+    
+    public var inputBuffer: UInt8 = 0
+    public var outputBuffer: UInt8 = 0
+    public var sck: UInt8 = 0
+    
     public var serialInput: [UInt8] = []
     public var serialOutput: [UInt8] = []
-//    public var payload: UInt8 = 0
-//    public var sck: UInt8 = 0
-//    public var addr: UInt8 = 0
 
-    var selectedRegister: Int {
-        return registerY.integerValue & 1
+    public var address: UInt16 {
+        set(address) {
+            registerX = Register(withValue: UInt8((address & 0xff) >> 8))
+            registerY = Register(withValue: UInt8(address & 0xff))
+        }
+        get {
+            return (UInt16(registerX.value) << 8) | UInt16(registerY.value)
+        }
     }
     
     public func provideSerialInput(bytes: [UInt8]) {
@@ -32,6 +58,8 @@ public class SerialInterfacePeripheral: ComputerPeripheral {
     }
 
     public init() {
+        microState = kMicroStateWaitingForRisingSCK
+        state = kStateIdle
         super.init(name: "Serial")
     }
     
@@ -47,23 +75,113 @@ public class SerialInterfacePeripheral: ComputerPeripheral {
         }
     }
     
+    public override func onPeripheralClock() {
+        tickMicroStateMachine()
+    }
+    
     func store(_ value: UInt8) {
-        serialOutput.append(value)
+        if address == kControlRegister {
+            sck = bus.value
+        } else {
+            inputBuffer = bus.value
+        }
     }
     
     func load(_ value: UInt8) {
-        if selectedRegister == 1 {
-            let value = UInt8(serialInput.count)
-            bus = Register(withValue: value)
-            return
+        bus = Register(withValue: outputBuffer)
+    }
+    
+    func tickMicroStateMachine() {
+        switch microState {
+        case kMicroStateWaitingForRisingSCK:
+            if sck != 0 {
+                microState = kMicroStateProcessing
+            }
+            
+        case kMicroStateWaitingForFallingSCK:
+            if sck == 0 {
+                microState = kMicroStateWaitingForRisingSCK
+            }
+            
+        case kMicroStateProcessing:
+            tickMacroStateMachine()
+            microState = kMicroStateWaitingForFallingSCK
+            
+        default:
+            assert(false)
         }
+    }
+    
+    func tickMacroStateMachine() {
+        var nextState = kStateIdle
+        switch (state) {
+        case kStateIdle:
+            nextState = doStateIdle(mosi: inputBuffer)
         
+        case kStateWaitingForOutputByte:
+            nextState = doStateWaiting(mosi: inputBuffer)
+              
+        default:
+              assert(false)
+        }
+        state = nextState
+    }
+    
+    func doStateIdle(mosi: UInt8) -> UInt8 {
+        var nextState = kStateIdle
+        switch (mosi) {
+        case kCommandResetSerialLink:
+            nextState = doCommandReset()
+
+        case kCommandPutByte:
+            nextState = doCommandPutByte()
+
+        case kCommandGetByte:
+            nextState = doCommandGetByte()
+
+        case kCommandGetNumBytes:
+            nextState = doCommandGetNumBytes()
+
+        default:
+            assert(false)
+        }
+        return nextState
+    }
+
+    func doCommandReset() -> UInt8 {
+        outputBuffer = kStatusSuccess
+        serialInput = []
+        serialOutput = []
+        return kStateIdle
+    }
+
+    func doCommandPutByte() -> UInt8 {
+        outputBuffer = kStatusSuccess
+        return kStateWaitingForOutputByte
+    }
+    
+    func doCommandGetByte() -> UInt8 {
+        outputBuffer = getNextByte()
+        return kStateIdle
+    }
+
+    func getNextByte() -> UInt8 {
         if let byte = serialInput.first {
             serialInput.removeFirst()
-            bus = Register(withValue: byte)
-            return
+            return byte
+        } else {
+            return 255
         }
-        
-        bus = Register(withValue: 0xff)
+    }
+
+    func doCommandGetNumBytes() -> UInt8 {
+        outputBuffer = UInt8(serialInput.count)
+        return kStateIdle
+    }
+    
+    func doStateWaiting(mosi: UInt8) -> UInt8 {
+        serialOutput.append(mosi)
+        outputBuffer = kStatusSuccess
+        return kStateIdle
     }
 }

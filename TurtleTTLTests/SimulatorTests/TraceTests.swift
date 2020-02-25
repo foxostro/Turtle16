6446ba9f0a5bb9900df4bf4c139ba967c956b7e7
foxostro <foxostro@gmail.com>
2020-02-24 21:31:39 -0800

    Trace now uses the guard statements on the Instruction object
    It no longer uses the Trace.Element enum to track this information.

--- TurtleTTLTests/SimulatorTests/TraceTests.swift
+++ TurtleTTLTests/SimulatorTests/TraceTests.swift
@@ -12,59 +12,44 @@ import TurtleTTL
 class TraceTests: XCTestCase {
     func testTraceIsInitiallyEmpty() {
         let trace = Trace()
-        XCTAssertEqual(trace.elements.count, 0)
+        XCTAssertTrue(trace.instructions.isEmpty)
     }
     
     func testAppendInstruction() {
         let trace = Trace()
-        trace.append(pc: ProgramCounter(withValue: 0), instruction: Instruction(opcode: 1, immediate: 1))
-        XCTAssertEqual(trace.elements.count, 1)
-        switch trace.elements.first! {
-        case .instruction(let pc, let ins):
-            XCTAssertEqual(pc.value, 0)
-            XCTAssertEqual(ins, Instruction(opcode: 1, immediate: 1))
-        default:
-            XCTFail()
-        }
+        trace.append(instruction: Instruction(opcode: 1, immediate: 1))
+        XCTAssertEqual(trace.instructions.count, 1)
+        XCTAssertEqual(trace.instructions.first?.pc.value, Optional(0))
+        XCTAssertEqual(trace.instructions.first, Optional(Instruction(opcode: 1, immediate: 1)))
     }
         
     func testAppendGuardConditionFlags() {
         let trace = Trace()
-        trace.appendGuard(pc: ProgramCounter(withValue: 0), flags: Flags(1, 1))
-        switch trace.elements.first! {
-        case .guardFlags(let pc, let flags):
-            XCTAssertEqual(pc.value, 0)
-            XCTAssertEqual(flags, Flags(1, 1))
-        default:
-            XCTFail()
-        }
+        trace.appendGuard(pc: ProgramCounter(withValue: 0),
+                          flags: Flags(1, 1),
+                          address: 0xcafe)
+        XCTAssertEqual(trace.instructions.count, 1)
+        XCTAssertEqual(trace.instructions.first?.pc.value, Optional(0))
+        XCTAssertEqual(trace.instructions.first?.guardFlags, Optional(Flags(1, 1)))
+        XCTAssertEqual(trace.instructions.first?.guardAddress, Optional(0xcafe))
     }
         
     func testAppendGuardConditionAddressRegister() {
         let trace = Trace()
-        trace.appendGuard(pc: ProgramCounter(withValue: 0), address: 0xFFFF)
-        switch trace.elements.first! {
-        case .guardAddress(let pc, let address):
-            XCTAssertEqual(pc.value, 0)
-            XCTAssertEqual(address, 0xFFFF)
-        default:
-            XCTFail()
-        }
+        trace.appendGuard(pc: ProgramCounter(withValue: 0),
+                          address: 0xcafe)
+        XCTAssertEqual(trace.instructions.count, 1)
+        XCTAssertEqual(trace.instructions.first?.pc.value, Optional(0))
+        XCTAssertEqual(trace.instructions.first?.guardAddress, Optional(0xcafe))
     }
     
     func testLogTrace() {
         let trace = Trace()
-        trace.append(pc: ProgramCounter(withValue: 0), instruction: makeNOP())
-        trace.appendGuard(pc: ProgramCounter(withValue: 1), flags: Flags(1, 1))
-        trace.appendGuard(pc: ProgramCounter(withValue: 1), address: 0xFFFF)
+        trace.append(instruction: Instruction.makeNOP())
+        trace.appendGuard(pc: ProgramCounter(withValue: 1), flags: Flags(1, 1), address: 0xFFFF)
         XCTAssertEqual(trace.description, """
-0x0000:\tNOP
-guard:\tflags={carryFlag: 1, equalFlag: 1}, traceExitingPC=0x0001
-guard:\taddress=0xffff, traceExitingPC=0x0001
+0x0000: NOP
+0x0001: NOP ; guardAddress=0xffff ; guardFlags={carryFlag: 1, equalFlag: 1}
 """)
-    }
-    
-    fileprivate func makeNOP() -> Instruction {
-        return Instruction(opcode: 0, immediate: 0, disassembly: "NOP")
     }
 }

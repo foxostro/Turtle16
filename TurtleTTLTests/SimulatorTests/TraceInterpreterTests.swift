0908105e5dae1b94901b474e3fc9bee8328fb0d8
foxostro <foxostro@gmail.com>
2020-02-24 21:47:18 -0800

    Add null logger to TraceInterpreterTests to increase test code coverage.

--- TurtleTTLTests/SimulatorTests/TraceInterpreterTests.swift
+++ TurtleTTLTests/SimulatorTests/TraceInterpreterTests.swift
@@ -12,6 +12,10 @@ import TurtleTTL
 class TraceInterpreterTests: XCTestCase {
     let isVerboseLogging = false
     
+    public class NullLogger: NSObject, Logger {
+        public func append(_ format: String, _ args: CVarArg...) {}
+    }
+    
     fileprivate func makeTraceInterpreter(cpuState: CPUStateSnapshot, program: String) -> TraceInterpreter {
         let trace = TraceUtils.recordTraceForProgram(program)
         let microcodeGenerator = MicrocodeGenerator()
@@ -20,7 +24,7 @@ class TraceInterpreterTests: XCTestCase {
                                                 peripherals: ComputerPeripherals(),
                                                 instructionDecoder: microcodeGenerator.microcode,
                                                 trace: trace)
-        traceInterpreter.logger = isVerboseLogging ? ConsoleLogger() : nil
+        traceInterpreter.logger = isVerboseLogging ? ConsoleLogger() : NullLogger()
         return traceInterpreter
     }
     

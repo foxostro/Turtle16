f8eeb19ae1e092805a07a097c7190b1ddcafa86b
foxostro <foxostro@gmail.com>
2020-03-05 00:05:41 -0800

    Add a bunch more ComputerExecutor tests.

--- Simulator/AppDelegate.swift
+++ Simulator/AppDelegate.swift
@@ -7,19 +7,15 @@
 //
 
 import Cocoa
-import TurtleTTL
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    public var executor: ComputerExecutor! = nil
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
-        executor.stop()
     }
     
     func applicationShouldTerminateAfterLastWindowClosed(_ app:NSApplication) -> Bool {

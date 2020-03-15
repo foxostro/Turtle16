3ae23e1ede742dde7b8e7a96eea944cf3fced2f4
foxostro <foxostro@gmail.com>
2020-03-14 22:21:51 -0700

    Add FlagsTest and a way to pack flags into a UInt8.

--- TurtleTTL/Simulator/Flags.swift
+++ TurtleTTL/Simulator/Flags.swift
@@ -10,10 +10,14 @@ import Cocoa
 
 // Represents a flags (condition codes) register in the TurtleTTL hardware.
 public class Flags: NSObject {
+    fileprivate let carryFlagMask: UInt8 = 0b00000001
+    fileprivate let equalFlagMask: UInt8 = 0b00000010
+    
     public let carryFlag: Int
     public let equalFlag: Int
+    
     public override var description: String {
-        return String(format:"{carryFlag: %d, equalFlag: %d}", carryFlag, equalFlag)
+        return "{carryFlag: \(carryFlag), equalFlag: \(equalFlag)}"
     }
     
     public override init() {
@@ -22,16 +26,35 @@ public class Flags: NSObject {
     }
     
     public init(_ carryFlag: Int, _ equalFlag: Int) {
+        assert(carryFlag == 0 || carryFlag == 1)
+        assert(equalFlag == 0 || equalFlag == 1)
         self.carryFlag = carryFlag
         self.equalFlag = equalFlag
     }
     
-    public override func isEqual(_ rhs: Any?) -> Bool {
-        if let rhs = rhs as? Flags {
-            return self == rhs
+    public init(value: UInt8) {
+        carryFlag = ((value & carryFlagMask) == 0) ? 0 : 1
+        equalFlag = ((value & equalFlagMask) == 0) ? 0 : 1
+    }
+    
+    public var value: UInt8 {
+        var value: UInt8 = 0
+        if carryFlag != 0 {
+            value |= carryFlagMask
         } else {
-            return false
+            value &= ~carryFlagMask
         }
+        if equalFlag != 0 {
+            value |= equalFlagMask
+        } else {
+            value &= ~equalFlagMask
+        }
+        return value
+    }
+    
+    public override func isEqual(_ rhs: Any?) -> Bool {
+        guard let rhs = rhs as? Flags else { return false }
+        return self == rhs
     }
 }
 

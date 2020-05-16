da8c05718cef2bfbda716211b7149e5aba1e7fe4
foxostro <foxostro@gmail.com>
2020-05-16 14:30:51 -0700

    Add TokenEqual class

---
+++ TurtleTTL/Assembler/Token/TokenLet.swift
@@ -0,0 +1,11 @@
+//
+//  TokenLet.swift
+//  TurtleTTL
+//
+//  Created by Andrew Fox on 5/16/20.
+//  Copyright © 2020 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+public class TokenLet: Token {}

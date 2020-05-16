2890f721aee1e12c2785ad589754ef932ddcc871
foxostro <foxostro@gmail.com>
2020-05-16 14:33:22 -0700

    Add ConstantDeclarationNode class and tests

---
+++ TurtleTTL/Assembler/AST/ConstantDeclarationNode.swift
@@ -0,0 +1,31 @@
+//
+//  ConstantDeclarationNode.swift
+//  TurtleTTL
+//
+//  Created by Andrew Fox on 5/16/20.
+//  Copyright © 2020 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+public class ConstantDeclarationNode: AbstractSyntaxTreeNode {
+    public let identifier: TokenIdentifier
+    public let number: TokenNumber
+    
+    public required init(identifier: TokenIdentifier, number: TokenNumber) {
+        self.identifier = identifier
+        self.number = number
+        super.init(children: [])
+    }
+    
+    public override func accept(visitor: AbstractSyntaxTreeNodeVisitor) throws {
+        try visitor.visit(node: self)
+    }
+    
+    public override func isEqual(_ rhs: Any?) -> Bool {
+        guard let rhs = rhs as? ConstantDeclarationNode else { return false }
+        guard identifier == rhs.identifier else { return false }
+        guard number == rhs.number else { return false }
+        return true
+    }
+}

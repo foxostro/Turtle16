8f9416703e59816075b6d240918854cd0e180440
foxostro <foxostro@gmail.com>
2020-05-17 00:53:11 -0700

    Fix retain cycle in AssemblerLexer.

--- TurtleAssemblerCore/AssemblerLexer.swift
+++ TurtleAssemblerCore/AssemblerLexer.swift
@@ -9,104 +9,105 @@
 public class AssemblerLexer: Lexer {
     public required init(withString string: String) {
         super.init(withString: string)
-        self.rules = [ // XXX: These closures retain self and self retains closures through rules. Retain cyle?
-            Rule(pattern: "\n") {
-                let token = TokenNewline(lineNumber: self.lineNumber, lexeme: $0)
-                self.lineNumber += 1
+        self.rules = [
+            Rule(pattern: "\n") {[weak self] in
+                let this = self!
+                let token = TokenNewline(lineNumber: this.lineNumber, lexeme: $0)
+                this.lineNumber += 1
                 return token
             },
-            Rule(pattern: "((#)|(//))") {(lexeme: String) in
-                self.advanceToNewline()
+            Rule(pattern: "((#)|(//))") {[weak self] (lexeme: String) in
+                self!.advanceToNewline()
                 return nil
             },
-            Rule(pattern: ",") {
-                TokenComma(lineNumber: self.lineNumber, lexeme: $0)
+            Rule(pattern: ",") {[weak self] in
+                TokenComma(lineNumber: self!.lineNumber, lexeme: $0)
             },
-            Rule(pattern: ":") {
-                TokenColon(lineNumber: self.lineNumber, lexeme: $0)
+            Rule(pattern: ":") {[weak self] in
+                TokenColon(lineNumber: self!.lineNumber, lexeme: $0)
             },
-            Rule(pattern: "A\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .A)
+            Rule(pattern: "A\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .A)
             },
-            Rule(pattern: "B\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .B)
+            Rule(pattern: "B\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .B)
             },
-            Rule(pattern: "C\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .C)
+            Rule(pattern: "C\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .C)
             },
-            Rule(pattern: "D\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .D)
+            Rule(pattern: "D\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .D)
             },
-            Rule(pattern: "E\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .E)
+            Rule(pattern: "E\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .E)
             },
-            Rule(pattern: "G\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .G)
+            Rule(pattern: "G\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .G)
             },
-            Rule(pattern: "H\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .H)
+            Rule(pattern: "H\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .H)
             },
-            Rule(pattern: "M\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .M)
+            Rule(pattern: "M\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .M)
             },
-            Rule(pattern: "P\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .P)
+            Rule(pattern: "P\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .P)
             },
-            Rule(pattern: "U\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .U)
+            Rule(pattern: "U\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .U)
             },
-            Rule(pattern: "V\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .V)
+            Rule(pattern: "V\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .V)
             },
-            Rule(pattern: "X\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .X)
+            Rule(pattern: "X\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .X)
             },
-            Rule(pattern: "Y\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .Y)
+            Rule(pattern: "Y\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .Y)
             },
-            Rule(pattern: "_\\b") {
-                TokenRegister(lineNumber: self.lineNumber, lexeme: $0, literal: .NONE)
+            Rule(pattern: "_\\b") {[weak self] in
+                TokenRegister(lineNumber: self!.lineNumber, lexeme: $0, literal: .NONE)
             },
-            Rule(pattern: "=") {
-                TokenEqual(lineNumber: self.lineNumber, lexeme: $0)
+            Rule(pattern: "=") {[weak self] in
+                TokenEqual(lineNumber: self!.lineNumber, lexeme: $0)
             },
-            Rule(pattern: "let") {
-                TokenLet(lineNumber: self.lineNumber, lexeme: $0)
+            Rule(pattern: "let") {[weak self] in
+                TokenLet(lineNumber: self!.lineNumber, lexeme: $0)
             },
-            Rule(pattern: "[_a-zA-Z][_a-zA-Z0-9]+\\b") {
-                TokenIdentifier(lineNumber: self.lineNumber, lexeme: $0)
+            Rule(pattern: "[_a-zA-Z][_a-zA-Z0-9]+\\b") {[weak self] in
+                TokenIdentifier(lineNumber: self!.lineNumber, lexeme: $0)
             },
-            Rule(pattern: "[-]{0,1}[0-9]+\\b") {
+            Rule(pattern: "[-]{0,1}[0-9]+\\b") {[weak self] in
                 let scanner = Scanner(string: $0)
                 var number: Int = 0
                 let result = scanner.scanInt(&number)
                 assert(result)
-                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: number)
+                return TokenNumber(lineNumber: self!.lineNumber, lexeme: $0, literal: number)
             },
-            Rule(pattern: "\\$[0-9a-fA-F]+\\b") {
+            Rule(pattern: "\\$[0-9a-fA-F]+\\b") {[weak self] in
                 let scanner = Scanner(string: String($0.dropFirst()))
                 var number: UInt32 = 0
                 let result = scanner.scanHexInt32(&number)
                 assert(result)
-                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: Int(number))
+                return TokenNumber(lineNumber: self!.lineNumber, lexeme: $0, literal: Int(number))
             },
-            Rule(pattern: "0[xX][0-9a-fA-F]+\\b") {
+            Rule(pattern: "0[xX][0-9a-fA-F]+\\b") {[weak self] in
                 let scanner = Scanner(string: String($0))
                 var number: UInt32 = 0
                 let result = scanner.scanHexInt32(&number)
                 assert(result)
-                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: Int(number))
+                return TokenNumber(lineNumber: self!.lineNumber, lexeme: $0, literal: Int(number))
             },
-            Rule(pattern: "0b[01]+\\b") {
+            Rule(pattern: "0b[01]+\\b") {[weak self] in
                 let scanner = Scanner(string: String($0))
                 var number = 0
                 let result = scanner.scanBinaryInt(&number)
                 assert(result)
-                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: number)
+                return TokenNumber(lineNumber: self!.lineNumber, lexeme: $0, literal: number)
             },
-            Rule(pattern: "'.'") {
+            Rule(pattern: "'.'") {[weak self] in
                 let number = Int(String($0).split(separator: "'").first!.unicodeScalars.first!.value)
-                return TokenNumber(lineNumber: self.lineNumber, lexeme: $0, literal: number)
+                return TokenNumber(lineNumber: self!.lineNumber, lexeme: $0, literal: number)
             },
             Rule(pattern: "[ \t]+") {(lexeme: String) in
                 nil

f1652e9d37f5520a10ed1486bb27dbeb59758f74
foxostro <foxostro@gmail.com>
2020-05-20 18:40:49 -0700

    Added a test for eval which executes the simulated program.
    Also, the Snap compiler now terminates programs with a HLT instruction.

--- SnapCoreTests/SnapCodeGeneratorTests.swift
+++ SnapCoreTests/SnapCodeGeneratorTests.swift
@@ -9,15 +9,38 @@
 import XCTest
 import SnapCore
 import TurtleCompilerToolbox
+import TurtleSimulatorCore
 import TurtleCore
 
 class SnapCodeGeneratorTests: XCTestCase {
+    let isVerboseLogging = false
     let kStackPointerAddressHi: UInt16 = 0x0000
     let kStackPointerAddressLo: UInt16 = 0x0001
     let kStackPointerInitialValue = 0x0000
     var kProgramPrologue = ""
+    let kProgramEpilogue = "HLT"
     
     var microcodeGenerator: MicrocodeGenerator!
+    
+    func execute(instructions: [Instruction]) -> Computer {
+        let computer = makeComputer()
+        computer.provideInstructions(instructions)
+        XCTAssertNoThrow(try computer.runUntilHalted())
+        return computer
+    }
+    
+    func makeComputer() -> Computer {
+        let computer = Computer()
+        let microcodeGenerator = MicrocodeGenerator()
+        microcodeGenerator.generate()
+        computer.provideMicrocode(microcode: microcodeGenerator.microcode)
+        computer.logger = makeLogger()
+        return computer
+    }
+    
+    func makeLogger() -> Logger {
+        return isVerboseLogging ? ConsoleLogger() : NullLogger()
+    }
     
     override func setUp() {
         microcodeGenerator = MicrocodeGenerator()
@@ -72,9 +95,18 @@ LI M, \((kStackPointerInitialValue & 0x00ff))
         return codeGenerator
     }
     
+    // Tacks the program epilogue and prologue onto the given assembly code.
+    func makeExpectedProgram(_ userProgram: String) -> String {
+        if userProgram == "" {
+            return kProgramPrologue + "\n" + kProgramEpilogue
+        } else {
+            return kProgramPrologue + "\n" + userProgram + "\n" + kProgramEpilogue
+        }
+    }
+    
     func testEmptyProgram() {
         let instructions = mustCompile(AbstractSyntaxTreeNode())
-        XCTAssertEqual(disassemble(instructions), kProgramPrologue)
+        XCTAssertEqual(disassemble(instructions), makeExpectedProgram(""))
     }
     
     func testFailToCompileDueToRedefinitionOfLabel() {
@@ -103,8 +135,27 @@ LI M, \((kStackPointerInitialValue & 0x00ff))
                           expression: Expression.Literal(number: TokenNumber(lineNumber: 1, lexeme: "1", literal: 1)))
         ])
         let instructions = mustCompile(ast)
-        XCTAssertEqual(disassemble(instructions), kProgramPrologue + "\n" + """
-LI A, 1
-""")
+        let expected = makeExpectedProgram("LI A, 1")
+        let actual = disassemble(instructions)
+        XCTAssertEqual(expected, actual)
+        let computer = execute(instructions: instructions)
+        XCTAssertEqual(computer.cpuState.registerA.value, 1)
     }
+        
+//    func testEvalStatementMovesLiteralValueIntoRegisterA() {
+//        let one = TokenNumber(lineNumber: 1, lexeme: "1", literal: 1)
+//        let two = TokenNumber(lineNumber: 1, lexeme: "2", literal: 2)
+//        let ast = AbstractSyntaxTreeNode(children: [
+//            EvalStatement(token: TokenEval(lineNumber: 1, lexeme: "eval"),
+//                          expression: Expression.Addition(left: Expression.Literal(number: one),
+//                                                          right: Expression.Literal(number: two)))
+//        ])
+//        let instructions = mustCompile(ast)
+//        XCTAssertEqual(disassemble(instructions), kProgramPrologue + "\n" + """
+//LI A, 1
+//""")
+//
+//        let computer = execute(program: instructions)
+//        XCTAssertEqual(computer.cpuState.registerA.value, 3)
+//    }
 }

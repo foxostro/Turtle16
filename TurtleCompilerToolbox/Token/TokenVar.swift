c80cb82a979c2f556cc8af9ec013373e7b174578
foxostro <foxostro@gmail.com>
2020-05-28 22:52:36 -0700

    Added new "static" and "var" tokens to the Snap lexer.

---
+++ TurtleCompilerToolbox/Token/TokenVar.swift
@@ -0,0 +1,9 @@
+//
+//  TokenVar.swift
+//  TurtleCompilerToolbox
+//
+//  Created by Andrew Fox on 5/28/20.
+//  Copyright © 2020 Andrew Fox. All rights reserved.
+//
+
+public class TokenVar: Token {}

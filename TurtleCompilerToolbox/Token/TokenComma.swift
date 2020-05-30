5d7d1a79fe87e58ee859749b332d9cce88b40f31
foxostro <foxostro@gmail.com>
2020-05-29 22:22:12 -0700

    Many tokens are classes with no body. Collect these into one file.

--- TurtleCompilerToolbox/Token/TokenComma.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TokenComma.swift
-//  TurtleCompilerToolbox
-//
-//  Created by Andrew Fox on 9/3/19.
-//  Copyright © 2019 Andrew Fox. All rights reserved.
-//
-
-public class TokenComma : Token {}
--- TurtleCompilerToolbox/Token/TokenParenLeft.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TokenParenLeft.swift
-//  TurtleCompilerToolbox
-//
-//  Created by Andrew Fox on 5/24/20.
-//  Copyright © 2020 Andrew Fox. All rights reserved.
-//
-
-public class TokenParenLeft : Token {}

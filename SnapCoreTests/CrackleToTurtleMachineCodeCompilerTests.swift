d7b52191185be8ddb584b1eac130adb58d5702c0
foxostro <foxostro@gmail.com>
2020-08-30 12:23:06 -0700

    Function calls will save/restore temporaries to preserve their values.
    Preserving the values of temporaries across a function call is necessary to
    allow functions to be used in expressions. Otherwise, the expressions
    evaluated within the function will invalidate temporaries being used by the
    calling expression.

--- SnapCoreTests/CrackleToTurtleMachineCodeCompilerTests.swift
+++ SnapCoreTests/CrackleToTurtleMachineCodeCompilerTests.swift
@@ -2203,7 +2203,6 @@ class CrackleToTurtleMachineCodeCompilerTests: XCTestCase {
             computer.dataRAM.store16(value: UInt16(dst), to: dstPtr)
             computer.dataRAM.store16(value: UInt16(src), to: srcPtr)
         }
-        executor.isVerboseLogging = true
         let computer = try! executor.execute(ir: [.copyWordsIndirectDestinationIndirectSource(dstPtr, srcPtr, 2)])
         XCTAssertEqual(computer.dataRAM.load16(from: dst), 0xcafe)
         XCTAssertEqual(computer.dataRAM.load16(from: src), 0xcafe)

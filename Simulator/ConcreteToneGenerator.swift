f6500e5cabec7cd192dd8bdfbb85fb456d745cf8
foxostro <foxostro@gmail.com>
2020-09-05 17:43:08 -0700

    Update the Simulator to simulate the Rev 3 sound card.

--- Simulator/ConcreteToneGenerator.swift
+++ Simulator/ConcreteToneGenerator.swift
@@ -8,48 +8,7 @@
 
 import TurtleSimulatorCore
 
-final class ConcreteToneGenerator: ToneGenerator {
-    private var isRunning = false
-    private let oscillator = Oscillator()
-    private let lock = NSLock()
-    
-    public var frequency: Double {
-        get {
-            return oscillator.frequency
-        }
-        set (value) {
-            oscillator.frequency = value
-        }
-    }
-    
-    private var _amplitude1 = 0.0
-    public var amplitude1: Double {
-        get {
-            lock.lock()
-            defer { lock.unlock() }
-            return _amplitude1
-        }
-        set (value) {
-            lock.lock()
-            _amplitude1 = value
-            lock.unlock()
-        }
-    }
-    
-    private var _amplitude2 = 0.0
-    public var amplitude2: Double {
-        get {
-            lock.lock()
-            defer { lock.unlock() }
-            return _amplitude2
-        }
-        set (value) {
-            lock.lock()
-            _amplitude2 = value
-            lock.unlock()
-        }
-    }
-    
+final class ConcreteToneGenerator: AudioRenderer, ToneGenerator {
     private var _directDrive = 0.0
     public var directDrive: Double {
         get {
@@ -64,51 +23,173 @@ final class ConcreteToneGenerator: ToneGenerator {
         }
     }
     
-    let sawtoothFn: Oscillator.TransferFunction = { theta in
-        return theta / (2.0 * .pi)
-    }
-    
-    let triangleFn: Oscillator.TransferFunction = { theta in
-        if theta > .pi {
-            return 1 - (theta / .pi)
-        } else {
-            return theta / .pi
+    private var _triangleWaveFrequency = 0.0
+    public var triangleWaveFrequency: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _triangleWaveFrequency
+        }
+        set (value) {
+            lock.lock()
+            _triangleWaveFrequency = value
+            lock.unlock()
         }
     }
     
-    func transferFunction(_ theta: Double) -> Double {
+    private var _triangleWaveAmplitude = 0.0
+    public var triangleWaveAmplitude: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _triangleWaveAmplitude
+        }
+        set (value) {
+            lock.lock()
+            _triangleWaveAmplitude = value
+            lock.unlock()
+        }
+    }
+    
+    private var _pulseWaveFrequency = 0.0
+    public var pulseWaveFrequency: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _pulseWaveFrequency
+        }
+        set (value) {
+            lock.lock()
+            _pulseWaveFrequency = value
+            lock.unlock()
+        }
+    }
+    
+    private var _pulseWaveModulation = 0.0
+    public var pulseWaveModulation: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _pulseWaveModulation
+        }
+        set (value) {
+            lock.lock()
+            _pulseWaveModulation = value
+            lock.unlock()
+        }
+    }
+    
+    private var _pulseWaveAmplitude = 0.0
+    public var pulseWaveAmplitude: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _pulseWaveAmplitude
+        }
+        set (value) {
+            lock.lock()
+            _pulseWaveAmplitude = value
+            lock.unlock()
+        }
+    }
+    
+    private var _noiseAmplitude = 0.0
+    public var noiseAmplitude: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _noiseAmplitude
+        }
+        set (value) {
+            lock.lock()
+            _noiseAmplitude = value
+            lock.unlock()
+        }
+    }
+    
+    private var _masterGain = 0.0
+    public var masterGain: Double {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+            return _masterGain
+        }
+        set (value) {
+            lock.lock()
+            _masterGain = value
+            lock.unlock()
+        }
+    }
+    
+    private var triangleWaveTheta = 0.0
+    private var pulseWaveTheta = 0.0
+    
+    override func render(_ inNumberFrames: UInt32, _ samples: UnsafeMutableBufferPointer<Float>) {
         lock.lock()
         defer { lock.unlock() }
-        return triangleFn(theta)*_amplitude1 + sawtoothFn(theta)*_amplitude2 + _directDrive
-    }
-    
-    init() {
-        oscillator.transferFunction = { [weak self] (theta: Double) in
-            return self?.transferFunction(theta) ?? 0.0
+        
+        let triangleWaveTheta_increment = 2.0 * .pi * _triangleWaveFrequency / sampleRate
+        let pulseWaveTheta_increment = 2.0 * .pi * _pulseWaveFrequency / sampleRate
+        
+        for frame in 0..<inNumberFrames {
+            samples[Int(frame)] = Float(unlockedTransferFunction())
+            
+            triangleWaveTheta += triangleWaveTheta_increment
+            if (triangleWaveTheta > 2.0 * .pi) {
+                triangleWaveTheta -= 2.0 * .pi
+            }
+            
+            pulseWaveTheta += pulseWaveTheta_increment
+            if (pulseWaveTheta > 2.0 * .pi) {
+                pulseWaveTheta -= 2.0 * .pi
+            }
         }
     }
     
-    func start() {
-        if !isRunning {
-            oscillator.start()
-            isRunning = true
-        }
+    func unlockedTransferFunction() -> Double {
+        let triangleWave = unlockedTriangleWaveTransferFunction()
+        let pulseWave = unlockedPulseWaveTransferFunction()
+        let noise = unlockedNoiseTransferFunction()
+        return (triangleWave + pulseWave + _directDrive + noise) * _masterGain
     }
     
-    func stop() {
-        if isRunning {
-            oscillator.stop()
-            isRunning = false
+    func unlockedTriangleWaveTransferFunction() -> Double {
+        let triangleWave: Double
+        if triangleWaveTheta > .pi {
+            triangleWave = 1 - (triangleWaveTheta / .pi)
+        } else {
+            triangleWave = triangleWaveTheta / .pi
         }
+        return triangleWave * _triangleWaveAmplitude
+    }
+    
+    func unlockedPulseWaveTransferFunction() -> Double {
+        let pulseWave: Double
+        if pulseWaveTheta > (.pi * _pulseWaveModulation) {
+            pulseWave = 1
+        } else {
+            pulseWave = 0
+        }
+        return pulseWave * _pulseWaveAmplitude
+    }
+    
+    func unlockedNoiseTransferFunction() -> Double {
+        return Double.random(in: 0.0 ... _noiseAmplitude)
     }
     
     func reset() {
         stop()
-        frequency = 0.0
         lock.lock()
-        _amplitude1 = 0.0
-        _amplitude2 = 0.0
         _directDrive = 0.0
+        _triangleWaveFrequency = 0.0
+        _triangleWaveAmplitude = 0.0
+        _pulseWaveFrequency = 0.0
+        _pulseWaveModulation = 0.0
+        _pulseWaveAmplitude = 0.0
+        _noiseAmplitude = 0.0
+        _masterGain = 0.0
+        pulseWaveTheta = 0.0
+        triangleWaveTheta = 0.0
         lock.unlock()
     }
 }

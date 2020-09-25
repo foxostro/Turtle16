f20d596f4597c679711da19f06e39627ebbd9ae2
foxostro <foxostro@gmail.com>
2020-09-24 20:46:37 -0700

    Add the beginning of a module system. Use it to import stdlib.
    Currently, the first time a module is imported, the compiler will load the
    module source code from file, parse it, and cache resulting AST. This AST
    is injected into the program at the site of the import statement.
    
    This is super crude, and only a little bit better than textual inclusion.
    However, it allows me to start iterating on the module system and syntax.

--- SnapCore/SnapCompiler.swift
+++ SnapCore/SnapCompiler.swift
@@ -16,58 +16,18 @@ public class SnapCompiler: NSObject {
     public var instructions: [Instruction] = []
     public let programDebugInfo = SnapDebugInfo()
     
-    public let kStandardLibrarySourceFileName = "stdlib"
-    private var kStandardLibraryText: String!
-    
     public private(set) var errors: [CompilerError] = []
     public var hasError:Bool {
         return errors.count != 0
-    }
-    
-    private func loadStandardLibraryIfNecessary() throws {
-        if kStandardLibraryText == nil {
-            kStandardLibraryText = try readStandardLibraryFromFile()
-        }
-    }
-    
-    func readStandardLibraryFromFile() throws -> String? {
-        let bundle = Bundle(for: type(of: self))
-        if let fileName = bundle.path(forResource: kStandardLibrarySourceFileName, ofType: "snap") {
-            do {
-                return try String(contentsOfFile: fileName)
-            } catch {
-                throw CompilerError(message: "failed to read standard library from file: \(fileName)")
-            }
-        } else {
-            throw CompilerError(message: "standard library file is missing: \(kStandardLibrarySourceFileName).snap")
-        }
     }
     
     public func compile(_ text: String) {
         return compile(program: text, base: 0x0000)
     }
     
-    public func compile(program originalText: String, base: Int) {
+    public func compile(program text: String, base: Int) {
         instructions = []
         errors = []
-        
-        // Until we have a module system, include the standard library by
-        // appending the source code of the standard library at the end.
-        let text: String
-        if isUsingStandardLibrary {
-            do {
-                try loadStandardLibraryIfNecessary()
-            } catch let error as CompilerError {
-                errors.append(error)
-                return
-            } catch {
-                errors.append(CompilerError(message: "unrecoverable error: \(error.localizedDescription)"))
-                return
-            }
-            text = originalText + "\n" + kStandardLibraryText
-        } else {
-            text = originalText
-        }
         
         // Lexer pass
         let lexer = SnapLexer(withString: text)
@@ -90,6 +50,7 @@ public class SnapCompiler: NSObject {
         // Compile the AST to IR code
         let snapToCrackleCompiler = SnapToCrackleCompiler()
         snapToCrackleCompiler.programDebugInfo = programDebugInfo
+        snapToCrackleCompiler.isUsingStandardLibrary = isUsingStandardLibrary
         snapToCrackleCompiler.compile(ast: ast)
         if snapToCrackleCompiler.hasError {
             errors = snapToCrackleCompiler.errors

2fcae8f90bb6ba18d29318180d4a54e9e83d160c
foxostro <foxostro@gmail.com>
2020-10-14 20:42:31 -0700

    Fix audio test programs so they work on real hardware.

--- Examples/test_audio.asm
+++ Examples/test_audio.asm
@@ -23,92 +23,10 @@ LI P, 153 # Corresponds to CV=3V which is A3.
 LI Y, kTriangleWaveAmplitude
 LI P, 0x80
 LI Y, kMasterGain
-LI P, 0x80
+LI P, 0xff
 
-LXY delay
-JALR
-NOP
-NOP
-
-# Go silent again and halt the computer.
-LI D, kAudioDevice
-LI X, 0
-LI Y, kMasterGain
-LI P, 0
-
-LI A, 0xff
-HLT
-
-
-
-
-delay256:
-
-LI A, 0
-delay256_0:
-# Increment A by 1. If the value is not equal to 255 then loop.
-LI B, 1
-ADD _
-ADD A
-LI B, 255
-CMP
-CMP
-LXY delay256_0
-JNE
-NOP
-NOP
-
-MOV X, G
-MOV Y, H
-JMP
-NOP
-NOP
-
-
-
-
-delay:
-
-# Preserve the value of the link register by
-# storing return address at address 0 and 1.
-LI U, 0
-LI V, 0
-MOV M, G
-LI V, 1
-MOV M, H
-
-LI A, 0
-delay_0:
-
-# Call delay256, making sure to preserve "A" in memory at address 2.
-LI U, 0
-LI V, 2
-MOV M, A
-LXY delay256
-JALR
-NOP
-NOP
-MOV A, M
-
-# Increment A by 1. If the value is not equal to 255 then loop.
-LI B, 1
-ADD _
-ADD A
-LI B, 10
-CMP
-CMP
-LXY delay_0
-JNE
-NOP
-NOP
-
-# Retrieve the return address from memory at address 0 and 1,
-# and then return from the call.
-LI U, 0
-LI V, 0
-MOV X, M
-LI V, 1
-MOV Y, M
+forever:
+LXY forever
 JMP
 NOP
 NOP

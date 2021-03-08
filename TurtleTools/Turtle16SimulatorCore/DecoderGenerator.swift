29ac4fa93d7c5638c72bbd6e248bc930e12a61fa
foxostro <foxostro@gmail.com>
2021-03-07 20:06:18 -0800

    The ADC and SBC instructions must be defined for all flag combinations.

--- TurtleTools/Turtle16SimulatorCore/DecoderGenerator.swift
+++ TurtleTools/Turtle16SimulatorCore/DecoderGenerator.swift
@@ -302,49 +302,49 @@ public class DecoderGenerator: NSObject {
                     makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: carry, z: z, ovf: 0, opcode: DecoderGenerator.opcodeBge), signals: signalsForRelativeJump)
                     makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 1, z: z, ovf: ovf, opcode: DecoderGenerator.opcodeBltu), signals: signalsForRelativeJump)
                     makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 0, z: z, ovf: ovf, opcode: DecoderGenerator.opcodeBgeu), signals: signalsForRelativeJump)
+                    
+                    makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 0, z: z, ovf: ovf, opcode: DecoderGenerator.opcodeAdc), signals: [
+                        DecoderGenerator.SelRightOp(0b00),
+                        DecoderGenerator.FI,
+                        DecoderGenerator.C0,
+                        DecoderGenerator.I2,
+                        DecoderGenerator.WriteBackSrc(.aluResult),
+                        DecoderGenerator.WRL,
+                        DecoderGenerator.WRH,
+                        DecoderGenerator.WBEN
+                    ])
+                    makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 1, z: z, ovf: ovf, opcode: DecoderGenerator.opcodeAdc), signals: [
+                        DecoderGenerator.SelRightOp(0b00),
+                        DecoderGenerator.FI,
+                        DecoderGenerator.I2,
+                        DecoderGenerator.WriteBackSrc(.aluResult),
+                        DecoderGenerator.WRL,
+                        DecoderGenerator.WRH,
+                        DecoderGenerator.WBEN
+                    ])
+                    
+                    makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 0, z: z, ovf: ovf, opcode: DecoderGenerator.opcodeSbc), signals: [
+                        DecoderGenerator.SelRightOp(0b00),
+                        DecoderGenerator.FI,
+                        DecoderGenerator.ALUI(0b010),
+                        DecoderGenerator.WriteBackSrc(.aluResult),
+                        DecoderGenerator.WRL,
+                        DecoderGenerator.WRH,
+                        DecoderGenerator.WBEN
+                    ])
+                    makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 1, z: z, ovf: ovf, opcode: DecoderGenerator.opcodeSbc), signals: [
+                        DecoderGenerator.SelRightOp(0b00),
+                        DecoderGenerator.FI,
+                        DecoderGenerator.C0,
+                        DecoderGenerator.ALUI(0b010),
+                        DecoderGenerator.WriteBackSrc(.aluResult),
+                        DecoderGenerator.WRL,
+                        DecoderGenerator.WRH,
+                        DecoderGenerator.WBEN
+                    ])
                 }
             }
         }
-        
-        makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 0, z: 0, ovf: 0, opcode: DecoderGenerator.opcodeAdc), signals: [
-            DecoderGenerator.SelRightOp(0b00),
-            DecoderGenerator.FI,
-            DecoderGenerator.C0,
-            DecoderGenerator.I2,
-            DecoderGenerator.WriteBackSrc(.aluResult),
-            DecoderGenerator.WRL,
-            DecoderGenerator.WRH,
-            DecoderGenerator.WBEN
-        ])
-        makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 1, z: 0, ovf: 0, opcode: DecoderGenerator.opcodeAdc), signals: [
-            DecoderGenerator.SelRightOp(0b00),
-            DecoderGenerator.FI,
-            DecoderGenerator.I2,
-            DecoderGenerator.WriteBackSrc(.aluResult),
-            DecoderGenerator.WRL,
-            DecoderGenerator.WRH,
-            DecoderGenerator.WBEN
-        ])
-        
-        makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 0, z: 0, ovf: 0, opcode: DecoderGenerator.opcodeSbc), signals: [
-            DecoderGenerator.SelRightOp(0b00),
-            DecoderGenerator.FI,
-            DecoderGenerator.ALUI(0b010),
-            DecoderGenerator.WriteBackSrc(.aluResult),
-            DecoderGenerator.WRL,
-            DecoderGenerator.WRH,
-            DecoderGenerator.WBEN
-        ])
-        makeControlWord(&controlWords, index: makeIndex(rst: 1, carry: 1, z: 0, ovf: 0, opcode: DecoderGenerator.opcodeSbc), signals: [
-            DecoderGenerator.SelRightOp(0b00),
-            DecoderGenerator.FI,
-            DecoderGenerator.C0,
-            DecoderGenerator.ALUI(0b010),
-            DecoderGenerator.WriteBackSrc(.aluResult),
-            DecoderGenerator.WRL,
-            DecoderGenerator.WRH,
-            DecoderGenerator.WBEN
-        ])
         
         return controlWords
     }

13d553f860d8aa259abe8e5d1d052a29949d49a8
foxostro <foxostro@gmail.com>
2021-04-05 09:20:35 -0700

    Rewrite logic in generateHazardControlSignals() to bear close correspodence to HDL needed to program the GAL.

--- TurtleTools/Turtle16SimulatorCore/ID.swift
+++ TurtleTools/Turtle16SimulatorCore/ID.swift
@@ -216,9 +216,6 @@ public class ID: NSObject {
     public var opcodeDecodeROM = Array<UInt>(repeating: 0, count: 512)
     
     public func step(input: Input) -> Output {
-        if input.ins == 0b0110100011100001 {
-            print("here")
-        }
         let hazardControl = generateHazardControlSignals(input: input)
         let ctl_EX: UInt = ((hazardControl.flush & 1)==1) ? ID.nopControlWord : decodeOpcode(input: input)
         let a = forwardA(input, hazardControl)
@@ -243,27 +240,58 @@ public class ID: NSObject {
     }
     
     fileprivate func generateHazardControlSignals(input: Input) -> HazardControlSignals {
-        // TODO: Rewrite this method in a style which can be closely transcribed into the GAL asm HDL language.
+        // The hazard control logic is written in this awkward way so that there
+        // is a close correspondence between this simulator code and the HDL
+        // used in U73, a ATF22V10, in the actual hardware.
         
-        if input.j & 1 == 0 {
-            return HazardControlSignals(flush: 1,
-                                        stall: 0,
-                                        fwd_a: 0,
-                                        fwd_ex_to_a: 1,
-                                        fwd_mem_to_a: 1,
-                                        fwd_b: 0,
-                                        fwd_ex_to_b: 1,
-                                        fwd_mem_to_b: 1)
-        }
+        let selA = splitOutSelA(input: input)
+        let selB = splitOutSelB(input: input)
+        let selC_EX: UInt = (input.ins_EX >> 8) & 0b111
+        let selC_MEM: UInt = input.selC_MEM
+        let writeBackSrc_EX: UInt = (input.ctl_EX >> 17) & 1
+        let writeBackSrc_MEM: UInt = (input.ctl_MEM >> 17) & 1
+        let wben_EX: UInt = (input.ctl_EX >> 20) & 1
+        let wben_MEM: UInt = (input.ctl_MEM >> 20) & 1
+        let j: UInt = input.j & 1
         
-        let resultStall = HazardControlSignals(flush: 1,
-                                               stall: 1,
-                                               fwd_a: 0,
-                                               fwd_ex_to_a: 1,
-                                               fwd_mem_to_a: 1,
-                                               fwd_b: 0,
-                                               fwd_ex_to_b: 1,
-                                               fwd_mem_to_b: 1)
+        // The hardware has an array of identity comparators to generate these
+        // signals.
+        let sel_a_matches_sel_c_ex: UInt  = (selA == selC_EX)  ? 0 : 1
+        let sel_b_matches_sel_c_ex: UInt  = (selB == selC_EX)  ? 0 : 1
+        let sel_a_matches_sel_c_mem: UInt = (selA == selC_MEM) ? 0 : 1
+        let sel_b_matches_sel_c_mem: UInt = (selB == selC_MEM) ? 0 : 1
+        
+        // For `fwd_a', we really want an expression like the following:
+        //   let fwd_a = ~((sel_a_matches_sel_c_ex | wben_EX | writeBackSrc_EX) & (sel_a_matches_sel_c_mem | wben_MEM | writeBackSrc_MEM)) & 1
+        // However, the ATF22V10 (equivalent to the venerable GAL22V10) is very
+        // particular about the way we program it. All equations must be defined
+        // as a series of terms ANDed together, and that list of terms is ORed
+        // together. There is support for taking the inverted value of the
+        // output, or of any individual input.
+        //
+        // The Swift compiler demands that we split this expression
+        // into separate subexpressions. We can't do this on the GAL.
+        let a: UInt = (sel_a_matches_sel_c_ex & sel_a_matches_sel_c_mem) | (sel_a_matches_sel_c_ex & wben_MEM) | (sel_a_matches_sel_c_ex & writeBackSrc_MEM)
+        let b: UInt = (wben_EX & sel_a_matches_sel_c_mem) | (wben_EX & wben_MEM) | (wben_EX & writeBackSrc_MEM)
+        let c: UInt = (writeBackSrc_EX & sel_a_matches_sel_c_mem) | (writeBackSrc_EX & wben_MEM) | (writeBackSrc_EX & writeBackSrc_MEM)
+        let fwd_a = ~(a | b | c) & 1
+        
+        // For `fwd_b', we really want an expression like the following:
+        //   let fwd_b = ~((sel_b_matches_sel_c_ex | wben_EX | writeBackSrc_EX) & (sel_b_matches_sel_c_mem | wben_MEM | writeBackSrc_MEM)) & 1
+        // However, we can't write it that way for the same reasons as `fwd_a'.
+        //
+        // The Swift compiler demands that we split this expression
+        // into separate subexpressions. We can't do this on the GAL.
+        let d: UInt = (sel_b_matches_sel_c_ex & sel_b_matches_sel_c_mem) | (sel_b_matches_sel_c_ex & wben_MEM) | (sel_b_matches_sel_c_ex & writeBackSrc_MEM)
+        let e: UInt = (wben_EX & sel_b_matches_sel_c_mem) | (wben_EX & wben_MEM) | (wben_EX & writeBackSrc_MEM)
+        let f: UInt = (writeBackSrc_EX & sel_b_matches_sel_c_mem) | (writeBackSrc_EX & wben_MEM) | (writeBackSrc_EX & writeBackSrc_MEM)
+        let fwd_b = ~(d | e | f) & 1
+        
+        let fwd_ex_to_a: UInt = (sel_a_matches_sel_c_ex | wben_EX | writeBackSrc_EX) & 1
+        let fwd_ex_to_b: UInt = (sel_b_matches_sel_c_ex | wben_EX | writeBackSrc_EX) & 1
+        
+        let fwd_mem_to_a: UInt = (sel_a_matches_sel_c_mem | wben_MEM | writeBackSrc_MEM) & 1
+        let fwd_mem_to_b: UInt = (sel_b_matches_sel_c_mem | wben_MEM | writeBackSrc_MEM) & 1
         
         // Determine whether the given opcode specifies an instruction that
         // depends on the ALU flags. This depends on the order of opcodes. The
@@ -274,84 +302,26 @@ public class ID: NSObject {
         let opcode4: UInt = UInt(input.ins) >> 15
         let areFlagsNeeded: UInt = opcode3 & opcode4
         let ctl_EX5: UInt = (input.ctl_EX >> 5) & 1
-        let isFlagsHazard = areFlagsNeeded & ~ctl_EX5
-        if isFlagsHazard != 0 {
-            return resultStall
-        }
+        let isFlagsHazard: UInt = (areFlagsNeeded & ~ctl_EX5) & 1
         
-        let selA = splitOutSelA(input: input)
-        let selB = splitOutSelB(input: input)
-        let selC_EX = (input.ins_EX >> 8) & 0b111
-        let wben_EX = (input.ctl_EX >> 20) & 1
-        let wben_MEM = (input.ctl_MEM >> 20) & 1
+        // The Swift compiler demands that we split this expression
+        // into separate subexpressions. We can't do this on the GAL.
+        let need_to_forward_storeOp_EX_to_a: UInt = (sel_a_matches_sel_c_ex | wben_EX | ~writeBackSrc_EX) & 1
+        let need_to_forward_storeOp_MEM_to_a: UInt = (sel_a_matches_sel_c_mem | wben_MEM | ~writeBackSrc_MEM) & 1
+        let need_to_forward_storeOp_EX_to_b: UInt = (sel_b_matches_sel_c_ex | wben_EX | ~writeBackSrc_EX) & 1
+        let need_to_forward_storeOp_MEM_to_b: UInt = (sel_b_matches_sel_c_mem | wben_MEM | ~writeBackSrc_MEM) & 1
+        let flush: UInt = ~(j & ~isFlagsHazard & need_to_forward_storeOp_EX_to_a & need_to_forward_storeOp_MEM_to_a & need_to_forward_storeOp_EX_to_b & need_to_forward_storeOp_MEM_to_b) & 1
         
-        enum RAWResolution {
-            case NoForwardingRequired, MustStall, Y_EX, Y_MEM
-        }
+        let stall = ~(~isFlagsHazard & need_to_forward_storeOp_EX_to_a & need_to_forward_storeOp_MEM_to_a & need_to_forward_storeOp_EX_to_b & need_to_forward_storeOp_MEM_to_b) & 1
         
-        let rawResolutionA: RAWResolution
-        let rawResolutionB: RAWResolution
-        
-        // Determine whether this is a RAW hazard involving A where the result we need is in EX.
-        if selA == selC_EX && wben_EX == 0 {
-            let writeBackSrc_EX = (input.ctl_EX >> 17) & 1
-            if writeBackSrc_EX == 0 {
-                // If the result we need is in Y_EX then we can forward it.
-                rawResolutionA = .Y_EX
-            } else {
-                // If the result we need is in storeOP_EX then we have to stall.
-                rawResolutionA = .MustStall
-            }
-        }
-        // Determine whether this is a RAW hazard involving A where the result we need is in MEM.
-        else if selA == input.selC_MEM && wben_MEM == 0 {
-            let writeBackSrc_MEM = (input.ctl_MEM >> 17) & 1
-            if writeBackSrc_MEM == 0 {
-                // If the result we need is in Y_MEM then we can forward it.
-                rawResolutionA = .Y_MEM
-            } else {
-                // If the result we need is in storeOP_MEM then we have to stall.
-                rawResolutionA = .MustStall
-            }
-        }
-        else {
-            rawResolutionA = .NoForwardingRequired
-        }
-        
-        // Determine whether this is a RAW hazard involving B where the result we need is in EX.
-        if selB == selC_EX && wben_EX == 0 {
-            let writeBackSrc_EX = (input.ctl_EX >> 17) & 1
-            if writeBackSrc_EX == 0 {
-                // If the result we need is in Y_EX then we can forward it.
-                rawResolutionB = .Y_EX
-            } else {
-                // If the result we need is in storeOP_EX then we have to stall.
-                rawResolutionB = .MustStall
-            }
-        }
-        // Determine whether this is a RAW hazard involving B where the result we need is in MEM.
-        else if selB == input.selC_MEM && wben_MEM == 0 {
-            let writeBackSrc_MEM = (input.ctl_MEM >> 17) & 1
-            if writeBackSrc_MEM == 0 {
-                // If the result we need is in Y_MEM then we can forward it.
-                rawResolutionB = .Y_MEM
-            } else {
-                // If the result we need is in storeOP_MEM then we have to stall.
-                rawResolutionB = .MustStall
-            }
-        }
-        else {
-            rawResolutionB = .NoForwardingRequired
-        }
-        
-        return HazardControlSignals(flush: (rawResolutionA == .MustStall || rawResolutionB == .MustStall) ? 1 : 0,
-                                    stall: (rawResolutionA == .MustStall || rawResolutionB == .MustStall) ? 1 : 0,
-                                    fwd_a: (rawResolutionA == .NoForwardingRequired || rawResolutionA == .MustStall) ? 0 : 1,
-                                    fwd_ex_to_a: (rawResolutionA == .Y_EX) ? 0 : 1,
-                                    fwd_mem_to_a: (rawResolutionA == .Y_MEM) ? 0 : 1,
-                                    fwd_b: (rawResolutionB == .NoForwardingRequired || rawResolutionB == .MustStall) ? 0 : 1,
-                                    fwd_ex_to_b: (rawResolutionB == .Y_EX) ? 0 : 1,
-                                    fwd_mem_to_b: (rawResolutionB == .Y_MEM) ? 0 : 1)
+        return HazardControlSignals(flush: flush,
+                                    stall: stall,
+                                    fwd_a: fwd_a,
+                                    fwd_ex_to_a: fwd_ex_to_a,
+                                    fwd_mem_to_a: fwd_mem_to_a,
+                                    fwd_b: fwd_b,
+                                    fwd_ex_to_b: fwd_ex_to_b,
+                                    fwd_mem_to_b: fwd_mem_to_b)
     }
     
     public func decodeOpcode(input: Input) -> UInt {

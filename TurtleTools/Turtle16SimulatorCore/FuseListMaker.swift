715f7af61fbff7bd9edb5bc749a286a9db8930ee
foxostro <foxostro@gmail.com>
2021-04-06 01:32:51 -0700

    Add a simulated model of the ATF22V10.
    The eventual goal is to define the behavior of the Program Counter and of
    the Control Unit through the actual compiled JEDEC fuse map files intended
    to be flashed to hardware.

---
+++ TurtleTools/Turtle16SimulatorCore/FuseListMaker.swift
@@ -0,0 +1,43 @@
+//
+//  FuseListMaker.swift
+//  Turtle16SimulatorCore
+//
+//  Created by Andrew Fox on 4/6/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Foundation
+
+// Tool for making a JEDEC fuse list. This is useful when simulating ATF22V10.
+public class FuseListMaker: NSObject {
+    public private(set) var fuseList: [UInt] = []
+    public var defaultFuseState: UInt = 0
+    
+    public var numberOfFuses: Int {
+        get {
+            return fuseList.count
+        }
+        
+        set (value) {
+            if value > fuseList.count {
+                fuseList += Array<UInt>(repeating: defaultFuseState, count: value - fuseList.count)
+            } else {
+                fuseList.removeLast(fuseList.count - value)
+            }
+        }
+    }
+    
+    public func set(begin: Int, array: [UInt]) {
+        assert(begin >= 0 && begin + array.count <= fuseList.count)
+        for i in 0..<array.count {
+            fuseList[begin+i] = array[i] & 1
+        }
+    }
+    
+    public func set(begin: Int, bitmap: String) {
+        set(begin: begin, array: bitmap.unicodeScalars.map({ (scalar) -> UInt in
+            assert(scalar == "0" || scalar == "1")
+            return scalar == "0" ? 0 : 1
+        }))
+    }
+}
---
+++ TurtleTools/Turtle16SimulatorCoreTests/ProductTermFuseMapTests.swift
@@ -0,0 +1,82 @@
+//
+//  ProductTermFuseMapTests.swift
+//  Turtle16SimulatorCoreTests
+//
+//  Created by Andrew Fox on 4/5/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+import Turtle16SimulatorCore
+
+class ProductTermFuseMapTests: XCTestCase {
+    func testSelectNoFusesSet() throws {
+        let map = ProductTermFuseMap(fuseListBitmap: 0b11111111111111111111111111111111111111111111)
+        let productTerm = map.evaluate(Array<UInt>(repeating: 0, count: 24))
+        XCTAssertEqual(productTerm, Array<UInt>(repeating:1, count: 44))
+    }
+    
+    func testSelectOneTerm_TrueInput() throws {
+        let map = ProductTermFuseMap(fuseListBitmap: 0b01111111111111111111111111111111111111111111)
+        let productTerm = map.evaluate(Array<UInt>(repeating: 0, count: 24))
+        XCTAssertEqual(productTerm, [0, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1])
+    }
+    
+    func testSelectOneTerm_InvInput() throws {
+        let map = ProductTermFuseMap(fuseListBitmap: 0b10111111111111111111111111111111111111111111)
+        let productTerm = map.evaluate([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        XCTAssertEqual(productTerm, [1, 0, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1])
+    }
+    
+    func testSelectTwoTerms() throws {
+        let map = ProductTermFuseMap(fuseListBitmap: 0b01110111111111111111111111111111111111111111)
+        let productTerm = map.evaluate([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        XCTAssertEqual(productTerm, [0, 1, 1, 1,
+                                     0, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1])
+    }
+    
+    func testSelectUpperTerms() throws {
+        let map = ProductTermFuseMap(fuseListBitmap: 0b01111011110111101111111111111111111111111111)
+        let productTerm = map.evaluate([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        XCTAssertEqual(productTerm, [0, 1, 1, 1,
+                                     1, 0, 1, 1,
+                                     1, 1, 0, 1,
+                                     1, 1, 1, 0,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1,
+                                     1, 1, 1, 1])
+    }
+}

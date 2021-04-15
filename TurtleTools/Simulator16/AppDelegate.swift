5f21e2afacf817570f5ca8a27a434e7528b8f589
foxostro <foxostro@gmail.com>
2021-04-15 08:50:05 -0700

    Add target for new Simulator16 app.
    Start with the empty app template. This will be fleshed out into a whole
    new simulator app for the Turtle16 hardware. The existing Simulator app
    can sit alongside this, undisturbed.

---
+++ TurtleTools/Simulator16/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Simulator16
+//
+//  Created by Andrew Fox on 4/15/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TurtleTools/Simulator16Tests/Simulator16Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Simulator16Tests.swift
+//  Simulator16Tests
+//
+//  Created by Andrew Fox on 4/15/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+@testable import Simulator16
+
+class Simulator16Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

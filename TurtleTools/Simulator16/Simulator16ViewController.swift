4da9456149d91e94f565b3b93df3fb90233f7da7
foxostro <foxostro@gmail.com>
2021-04-15 19:31:51 -0700

    Registers are displayed in a table now.

--- TurtleTools/Simulator16/Simulator16ViewController.swift
+++ TurtleTools/Simulator16/Simulator16ViewController.swift
@@ -19,7 +19,10 @@ class Simulator16ViewController: NSViewController {
     @IBOutlet var ovf: NSTextField!
     @IBOutlet var z: NSTextField!
     @IBOutlet var c0: NSTextField!
-    @IBOutlet var registers: NSTextField!
+    @IBOutlet var registerTableView: NSTableView!
+    
+    public let registerTableViewDataSource: RegisterTableViewDataSource
+    public let registerTableViewDelegate: RegisterTableViewDelegate
     
     public let computer: Turtle16Computer
     public let debugger: DebugConsole
@@ -27,6 +30,8 @@ class Simulator16ViewController: NSViewController {
     required init?(coder: NSCoder) {
         computer = Turtle16Computer(SchematicLevelCPUModel())
         debugger = DebugConsole(computer: computer)
+        registerTableViewDataSource = RegisterTableViewDataSource(computer: computer)
+        registerTableViewDelegate = RegisterTableViewDelegate()
         
         super.init(coder: coder)
     }
@@ -53,6 +58,9 @@ class Simulator16ViewController: NSViewController {
         debuggerOutput.string = "\n"
         debuggerOutput.font = debuggerInput.font
         
+        registerTableView.dataSource = registerTableViewDataSource
+        registerTableView.delegate = registerTableViewDelegate
+        
         reloadView()
     }
     
@@ -74,37 +82,6 @@ class Simulator16ViewController: NSViewController {
         ovf.isHidden = (computer.ovf == 0)
         z.isHidden = (computer.z == 0)
         c0.isHidden = (computer.carry == 0)
-        
-        let r0 = String(format: "%d", computer.getRegister(0))
-        let r1 = String(format: "%d", computer.getRegister(1))
-        let r2 = String(format: "%d", computer.getRegister(2))
-        let r3 = String(format: "%d", computer.getRegister(3))
-        let r4 = String(format: "%d", computer.getRegister(4))
-        let r5 = String(format: "%d", computer.getRegister(5))
-        let r6 = String(format: "%d", computer.getRegister(6))
-        let r7 = String(format: "%d", computer.getRegister(7))
-        let pc = String(format: "%d", computer.pc)
-        let hr0 = String(format: "0x%04x", computer.getRegister(0))
-        let hr1 = String(format: "0x%04x", computer.getRegister(1))
-        let hr2 = String(format: "0x%04x", computer.getRegister(2))
-        let hr3 = String(format: "0x%04x", computer.getRegister(3))
-        let hr4 = String(format: "0x%04x", computer.getRegister(4))
-        let hr5 = String(format: "0x%04x", computer.getRegister(5))
-        let hr6 = String(format: "0x%04x", computer.getRegister(6))
-        let hr7 = String(format: "0x%04x", computer.getRegister(7))
-        let hpc = String(format: "0x%04x", computer.pc)
-        registers.stringValue = """
-Name    Hex       Decimal
-r0      \(hr0)    \(r0)
-r1      \(hr1)    \(r1)
-r2      \(hr2)    \(r2)
-r3      \(hr3)    \(r3)
-r4      \(hr4)    \(r4)
-r5      \(hr5)    \(r5)
-r6      \(hr6)    \(r6)
-r7      \(hr7)    \(r7)
-pc      \(hpc)    \(pc)
-"""
-        registers.font = debuggerInput.font
+        registerTableView.reloadData()
     }
 }

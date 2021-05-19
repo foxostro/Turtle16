04ffb9b46a2b953247cd7bf0976f14adadd59fba
foxostro <foxostro@gmail.com>
2021-05-19 10:16:27 -0700

    Add new source files (basically empty) to be part of a new assembler

---
+++ TurtleTools/Turtle16SimulatorCore/AssemblerSingleInstructionCodeGenerator.swift
@@ -0,0 +1,13 @@
+//
+//  AssemblerSingleInstructionCodeGenerator.swift
+//  Turtle16SimulatorCore
+//
+//  Created by Andrew Fox on 5/17/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class AssemblerSingleInstructionCodeGenerator: NSObject {
+
+}
---
+++ TurtleTools/Turtle16SimulatorCoreTests/AssemblerParserTests.swift
@@ -0,0 +1,13 @@
+//
+//  AssemblerParserTests.swift
+//  Turtle16SimulatorCoreTests
+//
+//  Created by Andrew Fox on 5/16/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class AssemblerParserTests: XCTestCase {
+
+}

067c91b8c068eaaa250d9f6f0d1e91a2be7f2385
foxostro <foxostro@gmail.com>
2021-05-19 10:17:03 -0700

    Refactor by extracting methods in DebugConsoleCommandLineLexer

--- TurtleTools/TurtleCore/Lexer.swift
+++ TurtleTools/TurtleCore/Lexer.swift
@@ -132,4 +132,108 @@ open class Lexer: NSObject {
     func unexpectedCharacterError(sourceAnchor: SourceAnchor) -> CompilerError {
         return CompilerError(sourceAnchor: sourceAnchor, message: "unexpected character: `\(sourceAnchor.text)'")
     }
+    
+    public func makeNewlineRule() -> Lexer.Rule {
+        return Rule(pattern: "\n") {
+            TokenNewline(sourceAnchor: $0)
+        }
+    }
+    
+    public func makeCommaRule() -> Lexer.Rule {
+        return Rule(pattern: ",") {
+            TokenComma(sourceAnchor: $0)
+        }
+    }
+    
+    public func makeColonRule() -> Lexer.Rule {
+        return Rule(pattern: ":") {
+            TokenColon(sourceAnchor: $0)
+        }
+    }
+    
+    public func makeForwardSlashRule() -> Lexer.Rule {
+        return Rule(pattern: "/") {
+            TokenForwardSlash(sourceAnchor: $0)
+        }
+    }
+    
+    public func makeQuotedStringRule() -> Lexer.Rule {
+        return Rule(pattern: "\".*\"") {[weak self] in
+            TokenLiteralString(sourceAnchor: $0, literal: self!.interpretQuotedString(lexeme: String($0.text)))
+        }
+    }
+    
+    fileprivate func interpretQuotedString(lexeme: String) -> String {
+        var result = String(lexeme.dropFirst().dropLast())
+        let map = ["\0" : "\\0",
+                   "\t" : "\\t",
+                   "\n" : "\\n",
+                   "\r" : "\\r",
+                   "\"" : "\\\"",
+                   "\'" : "\\\'",
+                   "\\" : "\\\\"]
+        for (entity, description) in map {
+            result = result.replacingOccurrences(of: description, with: entity)
+        }
+        return result
+    }
+    
+    public func makeIdentifierRule() -> Lexer.Rule {
+        return Rule(pattern: "[_a-zA-Z][\\-_a-zA-Z0-9]*\\b") {
+            TokenIdentifier(sourceAnchor: $0)
+        }
+    }
+    
+    public func makeDecimalNumberRule() -> Lexer.Rule {
+        return Rule(pattern: "[-]{0,1}[0-9]+\\b") {
+            let scanner = Scanner(string: String($0.text))
+            var number: Int = 0
+            let result = scanner.scanInt(&number)
+            assert(result)
+            return TokenNumber(sourceAnchor: $0, literal: number)
+        }
+    }
+    
+    public func makeHexadecimalNumberWithDollarSigilRule() -> Lexer.Rule {
+        return Rule(pattern: "\\$[0-9a-fA-F]+\\b") {
+            let scanner = Scanner(string: String($0.text.dropFirst()))
+            var number: UInt64 = 0
+            let result = scanner.scanHexInt64(&number)
+            assert(result)
+            return TokenNumber(sourceAnchor: $0, literal: Int(number))
+        }
+    }
+    
+    public func makeHexadecimalNumberRule() -> Lexer.Rule {
+        return Rule(pattern: "0[xX][0-9a-fA-F]+\\b") {
+            let scanner = Scanner(string: String($0.text))
+            var number: UInt64 = 0
+            let result = scanner.scanHexInt64(&number)
+            assert(result)
+            return TokenNumber(sourceAnchor: $0, literal: Int(number))
+        }
+    }
+    
+    public func makeBinaryNumberRule() -> Lexer.Rule {
+        return Rule(pattern: "0b[01]+\\b") {
+            let scanner = Scanner(string: String($0.text))
+            var number = 0
+            let result = scanner.scanBinaryInt(&number)
+            assert(result)
+            return TokenNumber(sourceAnchor: $0, literal: number)
+        }
+    }
+    
+    public func makeQuotedCharacterRule() -> Lexer.Rule {
+        return Rule(pattern: "'.'") {
+            let number = Int(String($0.text).split(separator: "'").first!.unicodeScalars.first!.value)
+            return TokenNumber(sourceAnchor: $0, literal: number)
+        }
+    }
+    
+    public func makeWhitespaceRule() -> Lexer.Rule {
+        return Rule(pattern: "[ \t]+") {_ in
+            nil
+        }
+    }
 }

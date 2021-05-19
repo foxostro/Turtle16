11200319f5ef3a605464c12e8295d1272cb49537
foxostro <foxostro@gmail.com>
2021-05-19 10:16:27 -0700

    Add new source files (basically empty) to be part of a new assembler

---
+++ TurtleTools/Turtle16SimulatorCore/AssemblerLexer.swift
@@ -0,0 +1,13 @@
+//
+//  AssemblerLexer.swift
+//  Turtle16SimulatorCore
+//
+//  Created by Andrew Fox on 5/16/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class AssemblerLexer: Lexer {
+
+}
---
+++ TurtleTools/Turtle16SimulatorCoreTests/AssemblerCodeGeneratorTests.swift
@@ -0,0 +1,13 @@
+//
+//  AssemblerCodeGeneratorTests.swift
+//  Turtle16SimulatorCoreTests
+//
+//  Created by Andrew Fox on 5/17/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+
+class AssemblerCodeGeneratorTests: XCTestCase {
+
+}

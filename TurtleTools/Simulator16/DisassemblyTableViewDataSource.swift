254deba9483e6ee7a9c553c8c0dd8b985972b7e1
foxostro <foxostro@gmail.com>
2021-06-07 19:27:33 -0700

    Add a disassembly view to the Simulator app.

---
+++ TurtleTools/Simulator16/DisassemblyTableViewDataSource.swift
@@ -0,0 +1,67 @@
+//
+//  DisassemblyTableViewDataSource.swift
+//  Simulator16
+//
+//  Created by Andrew Fox on 6/7/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import Cocoa
+import Turtle16SimulatorCore
+
+class DisassemblyTableViewDataSource: NSObject, NSTableViewDataSource {
+    public let computer: Turtle16Computer
+    public private(set) var disassembly: [String] = []
+    public private(set) var labels: [Int : String] = [:]
+    
+    let kNumberOfRows = 65535
+    let kAddressIdentifier = NSUserInterfaceItemIdentifier("Address")
+    let kWordIdentifier = NSUserInterfaceItemIdentifier("Word")
+    let kLabelIdentifier = NSUserInterfaceItemIdentifier("Label")
+    let kMnemonicIdentifier = NSUserInterfaceItemIdentifier("Mnemonic")
+    
+    public init(computer: Turtle16Computer) {
+        self.computer = computer
+        super.init()
+        regenerateDisassembly()
+    }
+    
+    public func tableView(_ tableView: NSTableView,
+                          objectValueFor tableColumn: NSTableColumn?,
+                          row: Int) -> Any? {
+        switch tableColumn?.identifier {
+        case kAddressIdentifier:
+            return String(format: "%04x", row)
+            
+        case kWordIdentifier:
+            return String(format: "%04x", computer.instructions[row])
+            
+        case kLabelIdentifier:
+            if let label = labels[row] {
+                return label + ":"
+            } else {
+                return ""
+            }
+            
+        case kMnemonicIdentifier:
+            if row < disassembly.count {
+                return disassembly[row]
+            } else {
+                return ""
+            }
+            
+        default:
+            return nil
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return kNumberOfRows
+    }
+    
+    func regenerateDisassembly() {
+        let disassembler = Disassembler()
+        disassembly = disassembler.disassemble(computer.instructions)
+        labels = disassembler.labels
+    }
+}

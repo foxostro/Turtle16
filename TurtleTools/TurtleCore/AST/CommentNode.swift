6c734b2fdf927850a30f572d788654cf3870e1a0
foxostro <foxostro@gmail.com>
2021-07-17 12:53:42 -0700

    Add CommentNode for when we need to represent a comment in the AST

---
+++ TurtleTools/TurtleCore/AST/CommentNode.swift
@@ -0,0 +1,36 @@
+//
+//  CommentNode.swift
+//  TurtleCore
+//
+//  Created by Andrew Fox on 7/16/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+// Sometimes, such as during source to source translation, we need to represent a comment in the AST.
+public class CommentNode: AbstractSyntaxTreeNode {
+    public let string: String
+    
+    public convenience init(string: String) {
+        self.init(sourceAnchor: nil, string: string)
+    }
+    
+    public required init(sourceAnchor: SourceAnchor?, string: String) {
+        self.string = string
+        super.init(sourceAnchor: sourceAnchor)
+    }
+    
+    public override func isEqual(_ rhs: Any?) -> Bool {
+        guard rhs != nil else { return false }
+        guard type(of: rhs!) == type(of: self) else { return false }
+        guard let rhs = rhs as? CommentNode else { return false }
+        guard string == rhs.string else { return false }
+        return true
+    }
+    
+    public override var hash: Int {
+        var hasher = Hasher()
+        hasher.combine(string)
+        hasher.combine(super.hash)
+        return hasher.finalize()
+    }
+}

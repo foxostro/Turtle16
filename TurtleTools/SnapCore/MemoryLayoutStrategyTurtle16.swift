143fcaa3b24005b3458c359bf102a59aefe6ebe8
foxostro <foxostro@gmail.com>
2021-08-15 20:55:41 -0700

    Add MemoryLayoutStrategyTurtle16

---
+++ TurtleTools/SnapCore/MemoryLayoutStrategyTurtle16.swift
@@ -0,0 +1,46 @@
+//
+//  MemoryLayoutStrategyTurtle16.swift
+//  SnapCore
+//
+//  Created by Andrew Fox on 8/15/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+public class MemoryLayoutStrategyTurtle16: NSObject, MemoryLayoutStrategy {
+    public func sizeof(type: SymbolType) -> Int {
+        switch type {
+        case .compTimeInt, .compTimeBool, .void, .function:
+            return 0
+        case .constU8, .u8, .bool, .constBool:
+            return 1
+        case .constU16, .u16:
+            return 1
+        case .constPointer, .pointer:
+            return 1
+        case .constDynamicArray(elementType: _), .dynamicArray(elementType: _), .traitType(_):
+            return 2
+        case .array(count: let count, elementType: let elementType):
+            return (count ?? 0) * sizeof(type: elementType)
+        case .constStructType(let typ), .structType(let typ):
+            return sizeof(struct: typ)
+        case .unionType(let typ):
+            return sizeof(union: typ)
+        }
+    }
+    
+    func sizeof(struct typ: StructType) -> Int {
+        var accum = 0
+        for (_, symbol) in typ.symbols.symbolTable {
+            accum += sizeof(type: symbol.type)
+        }
+        return accum
+    }
+    
+    func sizeof(union typ: UnionType) -> Int {
+        let kTagSize = sizeof(type: .u8)
+        let kBufferSize = typ.members.reduce(0) { (result, memberType) -> Int in
+            return max(result, sizeof(type: memberType))
+        }
+        return kTagSize + kBufferSize
+    }
+}
---
+++ TurtleTools/SnapCoreTests/MemoryLayoutStrategyTurtle16Tests.swift
@@ -0,0 +1,66 @@
+//
+//  MemoryLayoutStrategyTurtle16Tests.swift
+//  SnapCoreTests
+//
+//  Created by Andrew Fox on 8/15/21.
+//  Copyright © 2021 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+import SnapCore
+
+class MemoryLayoutStrategyTurtle16Tests: XCTestCase {
+    func makeStrategy() -> MemoryLayoutStrategy {
+        return MemoryLayoutStrategyTurtle16()
+    }
+    
+    func testSizeOfVoid() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .void), 0)
+    }
+    
+    func testSizeOfU8() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .u8), 1)
+    }
+    
+    func testSizeOfU16() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .u16), 1)
+    }
+    
+    func testSizeOfPointer() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .pointer(.void)), 1)
+    }
+    
+    func testSizeOfDynamicArray() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .dynamicArray(elementType: .void)), 2)
+    }
+    
+    func testSizeOfArray() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .array(count: 10, elementType: .u8)), 10)
+    }
+    
+    func testSizeOfArrayWithIndeterminateSize() throws {
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: .array(count: nil, elementType: .u8)), 0)
+    }
+    
+    func testSizeOfUnion() throws {
+        let typ: SymbolType = .unionType(UnionType([.u16]))
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: typ), 2)
+    }
+    
+    func testSizeOfStruct() throws {
+        let typ: SymbolType = .structType(StructType(name: "Slice", symbols: SymbolTable(tuples: [
+            ("base", Symbol(type: .u16, offset: 0, storage: .automaticStorage)),
+            ("count", Symbol(type: .u16, offset: 1, storage: .automaticStorage))
+        ])))
+        let strategy = makeStrategy()
+        XCTAssertEqual(strategy.sizeof(type: typ), 2)
+    }
+}

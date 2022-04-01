1440c1acf232d7f6cb8aa94fa3647ddd41e4629f
foxostro <foxostro@gmail.com>
2022-04-01 01:33:44 -0700

    Add testVtableDemo

--- TurtleTools/SnapCoreTests/SnapToTurtle16CompilerTests.swift
+++ TurtleTools/SnapCoreTests/SnapToTurtle16CompilerTests.swift
@@ -2006,4 +2006,64 @@ func foo() {
         
         XCTAssertEqual(debugger?.loadSymbolU8("baz"), 116)
     }
+    
+    func testVtableDemo() {
+        var serialOutput: [UInt8] = []
+        let onSerialOutput = { (value: UInt16) in
+            serialOutput.append(UInt8(value & 0x00ff))
+        }
+        let options = Options(shouldDefineCompilerIntrinsicFunctions: true,
+                              runtimeSupport: kRuntime,
+                              shouldRunSpecificTest: "call through vtable pseudo-interface",
+                              onSerialOutput: onSerialOutput)
+        _ = run(options: options, program: """
+            public struct Serial {
+                print: func (*Serial, []const u8) -> void
+            }
+
+            public struct SerialFake {
+                vtable: Serial,
+                buffer: [64]u8,
+                cursor: u16
+            }
+
+            impl SerialFake {
+                func init() -> SerialFake {
+                    var serial: SerialFake = undefined
+                    serial.cursor = 0
+                    for i in 0..(serial.buffer.count) {
+                        serial.buffer[i] = 0
+                    }
+                    serial.vtable.print = &serial.print_ bitcastAs func (*Serial, []const u8) -> void
+                    return serial
+                }
+
+                func asSerial(self: *SerialFake) -> *Serial {
+                    return self bitcastAs *Serial
+                }
+
+                func print_(self: *SerialFake, s: []const u8) {
+                    for i in 0..(s.count) {
+                        self.buffer[self.cursor + i] = s[i]
+                    }
+                    self.cursor = self.cursor + s.count
+                }
+            }
+
+            test "call through vtable pseudo-interface" {
+                var serialFake = SerialFake.init()
+                let serial = serialFake.asSerial()
+                serial.print("test")
+                assert(serialFake.cursor == 4)
+                assert(serialFake.buffer[0] == 't')
+                assert(serialFake.buffer[1] == 'e')
+                assert(serialFake.buffer[2] == 's')
+                assert(serialFake.buffer[3] == 't')
+            }
+
+            """)
+        
+        let str = String(bytes: serialOutput, encoding: .utf8)
+        XCTAssertEqual(str, "passed\n")
+    }
 }

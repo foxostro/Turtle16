2eea08d717163e15ebfd8f2a742849f6ac44fcdc
foxostro <foxostro@gmail.com>
2022-04-26 08:57:10 -0700

    Remove LogicalDecoder because the Swift compiler chokes on it
    Remove LogicalDecoder. The Swift compiler cannot compile LogicalDecoder
    in Release mode and appears to hang forever. (Debug mode is fine.)

--- TurtleTools/Turtle16SimulatorCoreTests/DecoderTests.swift
+++ TurtleTools/Turtle16SimulatorCoreTests/DecoderTests.swift
@@ -9,48 +9,6 @@
 import XCTest
 import Turtle16SimulatorCore
 
-class DecoderTests: XCTestCase {
-    fileprivate func doesSignalMatchReference(_ signal: Int) -> Bool {
-        let logic = LogicalDecoder()
-        let lookupTable = OpcodeDecoderROM()
-        lookupTable.opcodeDecodeROM = DecoderGenerator().generate()
-        for i in 0..<logic.count {
-            let expected = (lookupTable.decode(i) >> signal) & 1
-            let actual = (logic.decode(i) >> signal) & 1
-            if expected != actual {
-                return false
-            }
-        }
-        return true
-    }
-
-    func testSignals() throws {
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.HLT))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.SelStoreOpA))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.SelStoreOpB))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.SelRightOpA))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.SelRightOpB))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.FI))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.C0))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.I0))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.I1))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.I2))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.RS0))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.RS1))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.J))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.JABS))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.MemLoad))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.MemStore))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.AssertStoreOp))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.WriteBackSrcFlag))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.WRL))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.WRH))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.WBEN))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.LeftOperandIsUnused))
-        XCTAssertTrue(doesSignalMatchReference(DecoderGenerator.RightOperandIsUnused))
-    }
-}
-
 class ProgrammableLogicDecoderTests: XCTestCase {
     fileprivate func doesSignalMatchReference(_ signal: Int, _ actual: [UInt], _ expected: [UInt]) -> Bool {
         for i in 0..<actual.count {

718cc2ed7eb557a05527c3feaeae5f99653c49c6
foxostro <foxostro@gmail.com>
2023-09-25 23:04:16 -0700

    Add EX module tests for ADD

--- EXModuleTestFixtureArduinoSketch/EXModuleTestFixtureArduinoSketch.ino
+++ EXModuleTestFixtureArduinoSketch/EXModuleTestFixtureArduinoSketch.ino
@@ -24,7 +24,7 @@ const LEDOutputPorts ledPorts = {
   .CLR = 14
 };
 
-void testSelC(unsigned ledState) {
+void testSelC() {
   TestFixtureOutputs testFixtureOutputs;
 
   printf("%s: ", __FUNCTION__);
@@ -42,7 +42,7 @@ void testSelC(unsigned ledState) {
   printf("passed\n");
 }
 
-void testControlWordCopiedToNextPipelineStage(unsigned ledState) {
+void testControlWordCopiedToNextPipelineStage() {
   TestFixtureOutputs testFixtureOutputs;
 
   printf("%s: ", __FUNCTION__);
@@ -63,7 +63,7 @@ void testControlWordCopiedToNextPipelineStage(unsigned ledState) {
 
 static const int kSelStoreOpShift = 1;
 
-void testStoreOp_RegisterB(unsigned ledState) {
+void testStoreOp_RegisterB() {
   TestFixtureOutputs testFixtureOutputs;
 
   printf("%s: ", __FUNCTION__);
@@ -83,7 +83,7 @@ void testStoreOp_RegisterB(unsigned ledState) {
   printf("passed\n");
 }
 
-void testStoreOp_PCPlusOne(unsigned ledState) {
+void testStoreOp_PCPlusOne() {
   TestFixtureOutputs testFixtureOutputs;
 
   printf("%s: ", __FUNCTION__);
@@ -103,7 +103,7 @@ void testStoreOp_PCPlusOne(unsigned ledState) {
   printf("passed\n");
 }
 
-void testStoreOp_Imm(unsigned ledState) {
+void testStoreOp_Imm() {
   TestFixtureOutputs testFixtureOutputs;
 
   printf("%s: ", __FUNCTION__);
@@ -123,7 +123,7 @@ void testStoreOp_Imm(unsigned ledState) {
   printf("passed\n");
 }
 
-void testStoreOp_ImmUpper(unsigned ledState) {
+void testStoreOp_ImmUpper() {
   TestFixtureOutputs testFixtureOutputs;
 
   printf("%s: ", __FUNCTION__);
@@ -143,14 +143,91 @@ void testStoreOp_ImmUpper(unsigned ledState) {
   printf("passed\n");
 }
 
-void (*allTests[])(unsigned) = {
+void testAdd() {
+  TestFixtureOutputs testFixtureOutputs;
+
+  printf("%s: ", __FUNCTION__);
+  printf("Add one and one...");
+
+  testFixtureOutputs = TestFixtureOutputs()
+    .ins(0)
+    .a(1)
+    .b(1)
+    .ctl(0b110110<<6);
+  testFixtureOutputPorts.set(testFixtureOutputs);
+
+  // Pulse the clock
+  testFixtureOutputs = testFixtureOutputPorts.tick(testFixtureOutputs);  
+  
+  TestFixtureInputs actualTestFixtureInputs = testFixtureInputPorts.read();
+  assertEqual(2, actualTestFixtureInputs.Y_EX, "Expect that Y_EX==2");
+  assertEqual(0, actualTestFixtureInputs.N, "Expect that N==0");
+  assertEqual(0, actualTestFixtureInputs.C, "Expect that C==0");
+  assertEqual(0, actualTestFixtureInputs.Z, "Expect that Z==0");
+  assertEqual(0, actualTestFixtureInputs.V, "Expect that V==0");
+  printf("passed\n");
+}
+
+void testAddWithCarryOutput() {
+  TestFixtureOutputs testFixtureOutputs;
+
+  printf("%s: ", __FUNCTION__);
+  printf("Add with carry output...");
+
+  testFixtureOutputs = TestFixtureOutputs()
+    .ins(0)
+    .a(0xffff)
+    .b(1)
+    .ctl(0b110110<<6);
+  testFixtureOutputPorts.set(testFixtureOutputs);
+
+  // Pulse the clock
+  testFixtureOutputs = testFixtureOutputPorts.tick(testFixtureOutputs);  
+  
+  TestFixtureInputs actualTestFixtureInputs = testFixtureInputPorts.read();
+  assertEqual(0, actualTestFixtureInputs.Y_EX, "Expect that Y_EX==0");
+  assertEqual(0, actualTestFixtureInputs.N, "Expect that N==0");
+  assertEqual(1, actualTestFixtureInputs.C, "Expect that C==1");
+  assertEqual(1, actualTestFixtureInputs.Z, "Expect that Z==1");
+  assertEqual(0, actualTestFixtureInputs.V, "Expect that V==0");
+  printf("passed\n");
+}
+
+void testAddWithSignedOverflow() {
+  TestFixtureOutputs testFixtureOutputs;
+
+  printf("%s: ", __FUNCTION__);
+  printf("Add with signed overflow...");
+
+  testFixtureOutputs = TestFixtureOutputs()
+    .ins(0)
+    .a(0x7fff)
+    .b(1)
+    .ctl(0b110110<<6);
+  testFixtureOutputPorts.set(testFixtureOutputs);
+
+  // Pulse the clock
+  testFixtureOutputs = testFixtureOutputPorts.tick(testFixtureOutputs);  
+  
+  TestFixtureInputs actualTestFixtureInputs = testFixtureInputPorts.read();
+  assertEqual(0x8000, actualTestFixtureInputs.Y_EX, "Expect that Y_EX==0x8000");
+  assertEqual(1, actualTestFixtureInputs.N, "Expect that N==1");
+  assertEqual(0, actualTestFixtureInputs.C, "Expect that C==0");
+  assertEqual(0, actualTestFixtureInputs.Z, "Expect that Z==0");
+  assertEqual(1, actualTestFixtureInputs.V, "Expect that V==1");
+  printf("passed\n");
+}
+
+void (*allTests[])(void) = {
   testSelC,
   testControlWordCopiedToNextPipelineStage,
   testStoreOp_RegisterB,
   testStoreOp_PCPlusOne,
   testStoreOp_Imm,
   testStoreOp_ImmUpper,
-  // #error("TODO: Write a test for NOP next")
+  testAdd,
+  testAddWithCarryOutput,
+  testAddWithSignedOverflow,
   // #error("TODO: Write a test for something which passes the right op through unmodified and exercise all options for SelRightOp")
   // #error("TODO: Write tests for all the arithmetic operations")
   // #error("TODO: Write tests for the status bits")
@@ -167,7 +244,7 @@ void doAllTests() {
   // Run all tests, one by one
   for (int i = 0, n = sizeof(allTests)/sizeof(*allTests); i < n; ++i) {
     chaser.step();
-    allTests[i](chaser.led);
+    allTests[i]();
   }
   for (int i = 0; i < 8; ++i) {
     delay(100);

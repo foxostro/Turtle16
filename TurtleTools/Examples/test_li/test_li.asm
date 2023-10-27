b323d8340168e51873b1f66250faa3c9def0bf40
foxostro <foxostro@gmail.com>
2023-10-26 20:31:24 -0700

    Update test_li test program

--- TurtleTools/Examples/test_li/test_li.asm
+++ TurtleTools/Examples/test_li/test_li.asm
@@ -1,6 +1,9 @@
 # Loads the value -1 into r7.
-# Arranges for the CPU to halt when this instruction enters the writeback stage.
+# Arranges for the CPU to halt after the LI instruction retires.
 NOP
 LI r7, -1
+NOP
+NOP
+NOP
 NOP
 HLT

2676ac335e3e88fae520ff93f990c3960d127845
foxostro <foxostro@gmail.com>
2023-11-21 11:47:53 -0800

    test_beq: ensure the control unit must stall to avoid a flags hazard

--- TurtleTools/Examples/test_beq/test_beq.asm
+++ TurtleTools/Examples/test_beq/test_beq.asm
@@ -1,15 +1,16 @@
 NOP
 LI r0, 1
-LI r1, 2
+LI r3, -2
 CMPI r0, 1
-NOP
 BEQ equal
-STORE r0, r0, 0
+LI r1, 1
+STORE r1, r3, 0
 NOP
 NOP
 HLT
 equal:
-STORE r1, r1, 0
+LI r1, 2
+STORE r1, r3, 0
 NOP
 NOP
 HLT

472fffa8514e1d6c2d9ed97fea3831441d2644dc
foxostro <foxostro@gmail.com>
2023-11-21 00:47:55 -0800

    Update fib test program to only run for five iterations
    This is the largest number of iterations for which I can capture the entire
    program execution on my oscilloscope.

--- TurtleTools/Examples/fib/fib.asm
+++ TurtleTools/Examples/fib/fib.asm
@@ -3,17 +3,14 @@
 LI r0, 0
 LI r1, 1
 LI r7, 0
-LI r6, -1
+LI r6, -2
 loop:
 ADD r2, r0, r1
 STORE r2, r6
 ADDI r0, r1, 0
 ADDI r7, r7, 1
 ADDI r1, r2, 0
-NOP
-NOP
-CMPI r7, 0
-NOP
+CMPI r7, 5
 NOP
 BLT loop
 HLT

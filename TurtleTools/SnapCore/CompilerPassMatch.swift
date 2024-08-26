d722454d2270492dca5d4386986fe05987effe22
foxostro <foxostro@gmail.com>
2024-08-25 22:45:58 -0700

    Add CompilerPassMatch
    CompilerPassMatch is a compiler pass which can lower and rewrite Match
    nodes in the AST in terms of lower-level nodes.

---
+++ TurtleTools/SnapCore/CompilerPassMatch.swift
@@ -0,0 +1,139 @@
+//
+//  CompilerPassMatch.swift
+//  SnapCore
+//
+//  Created by Andrew Fox on 8/18/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import TurtleCore
+
+// Discrete compiler pass to lower and erase Match statements
+public class CompilerPassMatch: CompilerPassWithDeclScan {
+    public override func visit(match node0: Match) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try super.visit(match: node0) as! Match
+        
+        let outer = SymbolTable(parent: symbols)
+        
+        let matchExprType = try RvalueExpressionTypeChecker(symbols: symbols!).check(expression: node1.expr)
+        
+        // Get the list of types this match statement is expected to contain.
+        let expectedTypes: NSOrderedSet
+        switch matchExprType {
+        case .unionType(let typ):
+            expectedTypes = NSOrderedSet(array: typ.members.map({ $0.correspondingMutableType }))
+        default:
+            expectedTypes = NSOrderedSet(array: [matchExprType.correspondingMutableType])
+        }
+        
+        // Check that the expected and provided types match.
+        let valueTypes = NSOrderedSet(array: try node1.clauses.map({
+            ($0, try RvalueExpressionTypeChecker(symbols: symbols!).check(expression: $0.valueType))
+        }))
+        let extraneousTypes = valueTypes.filter { (element_: Any) -> Bool in
+            let element = element_ as! (Match.Clause, SymbolType)
+            let isIncluded = !expectedTypes.contains(element.1)
+            return isIncluded
+        }
+        guard extraneousTypes.count == 0 else {
+            let what = extraneousTypes.map({"\(($0 as! (Match.Clause, SymbolType)).1)"}).joined(separator: ", ")
+            let clauseStr = extraneousTypes.count == 1 ? "clause" : "clauses"
+            let badClause = (extraneousTypes.first as? (Match.Clause, SymbolType))?.0
+            let sourceAnchor = badClause?.valueIdentifier.sourceAnchor?.union(badClause?.valueType.sourceAnchor)
+            throw CompilerError(sourceAnchor: sourceAnchor,
+                                message: "extraneous \(clauseStr) in match statement: \(what)")
+        }
+        let missingTypes = (expectedTypes.mutableCopy() as! NSMutableOrderedSet)
+        missingTypes.minus(NSOrderedSet(array: valueTypes.map {
+            ($0 as! (Match.Clause, SymbolType)).1
+        }))
+        guard missingTypes.count == 0 || node1.elseClause != nil else {
+            let what = missingTypes.map({"\($0)"}).joined(separator: ", ")
+            let clauseStr = missingTypes.count == 1 ? "clause" : "clauses"
+            let sourceAnchor = node1.expr.sourceAnchor
+            throw CompilerError(sourceAnchor: sourceAnchor,
+                                message: "match statement is not exhaustive. Missing \(clauseStr): \(what)")
+        }
+        
+        // Generate an AST for if-else tree to evaluate the clauses.
+        var stmts: [AbstractSyntaxTreeNode] = []
+        if !node1.clauses.isEmpty {
+            stmts.append(VarDeclaration(identifier: Expression.Identifier("__index"),
+                                        explicitType: nil,
+                                        expression: node1.expr,
+                                        storage: .automaticStorage,
+                                        isMutable: true))
+            stmts.append(compileMatchClause(node1, node1.clauses, outer))
+        }
+        else if let elseClause = node1.elseClause {
+            let rewrittenElseClause = Block(sourceAnchor: elseClause.sourceAnchor,
+                                            symbols: SymbolTable(parent: outer),
+                                            children: elseClause.children)
+            stmts.append(rewrittenElseClause)
+        }
+        
+        let block0 = Block(sourceAnchor: node1.sourceAnchor,
+                           symbols: outer,
+                           children: stmts)
+        
+        return block0
+    }
+    
+    fileprivate func compileMatchClause(_ match: Match, _ clauses: [Match.Clause], _ symbols: SymbolTable) -> If {
+        assert(!clauses.isEmpty)
+        
+        let clause = clauses.last!
+        let index = Expression.Identifier("__index")
+        
+        if clauses.count == 1 {
+            let clauseElseBlock: Block?
+            if match.elseClause == nil {
+                clauseElseBlock = nil
+            } else {
+                clauseElseBlock = Block(sourceAnchor: match.elseClause!.sourceAnchor,
+                                        symbols: SymbolTable(parent: symbols),
+                                        children: match.elseClause!.children)
+            }
+            
+            let outerSymbols = SymbolTable(parent: symbols)
+            
+            return If(condition: Expression.Is(expr: index, testType: clause.valueType),
+                      then: Block(symbols: outerSymbols,
+                                  children: [
+                        VarDeclaration(identifier: clause.valueIdentifier,
+                                       explicitType: nil,
+                                       expression: Expression.As(expr: index, targetType: clause.valueType),
+                                       storage: .automaticStorage,
+                                       isMutable: false),
+                                    Block(sourceAnchor: clause.block.sourceAnchor,
+                                          symbols: SymbolTable(parent: outerSymbols),
+                                          children: clause.block.children)
+                      ]),
+                      else: clauseElseBlock)
+        } else {
+            let outerSymbols = SymbolTable(parent: symbols)
+            return If(condition: Expression.Is(expr: index, testType: clause.valueType),
+                      then: Block(symbols: outerSymbols,
+                                  children: [
+                        VarDeclaration(identifier: clause.valueIdentifier,
+                                       explicitType: nil,
+                                       expression: Expression.As(expr: index, targetType: clause.valueType),
+                                       storage: .automaticStorage,
+                                       isMutable: false),
+                                    Block(sourceAnchor: clause.block.sourceAnchor,
+                                          symbols: SymbolTable(parent: outerSymbols),
+                                          children: clause.block.children)
+                      ]),
+                      else: compileMatchClause(match, clauses.dropLast(), symbols))
+        }
+    }
+}
+
+extension AbstractSyntaxTreeNode {
+    // Lower and rewrite Match statements
+    public func matchPass(_ globalEnvironment: GlobalEnvironment) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try self.clearSymbols(globalEnvironment)
+        let node2 = try CompilerPassMatch(globalEnvironment: globalEnvironment).run(node1)
+        return node2
+    }
+}
---
+++ TurtleTools/SnapCoreTests/CompilerPassMatchTests.swift
@@ -0,0 +1,200 @@
+//
+//  CompilerPassMatchTests.swift
+//  SnapCoreTests
+//
+//  Created by Andrew Fox on 8/18/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+import TurtleCore
+import SnapCore
+
+final class CompilerPassMatchTests: XCTestCase {
+    func testCompileEmptyMatchStatement() {
+        let symbols = SymbolTable(tuples: [
+            ("foo", Symbol(type: .arithmeticType(.mutableInt(.u8))))
+        ])
+        let input = Match(expr: Expression.Identifier("foo"), clauses: [], elseClause: nil)
+        
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        XCTAssertThrowsError(try compiler.run(input)) {
+            let compilerError = $0 as? CompilerError
+            XCTAssertNotNil(compilerError)
+            XCTAssertEqual(compilerError?.message, "match statement is not exhaustive. Missing clause: u8")
+        }
+    }
+    
+    func testCompileMatchStatementWithOnlyElseClause() throws {
+        let symbols = SymbolTable(tuples: [
+            ("result", Symbol(type: .arithmeticType(.mutableInt(.u8))))
+        ])
+        let input = Match(expr: Expression.Identifier("result"), clauses: [], elseClause: Block(children: [
+            Expression.Assignment(lexpr: Expression.Identifier("result"), rexpr: Expression.LiteralInt(42))
+        ]))
+        let expected = Block(children: [
+            Block(children: [
+                Expression.Assignment(lexpr: Expression.Identifier("result"),
+                                      rexpr: Expression.LiteralInt(42))
+            ])
+        ])
+        
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        let output = try compiler.run(input)
+        XCTAssertEqual(output, expected)
+    }
+
+    func testCompileMatchStatementWithOneExtraneousClause() {
+        let symbols = SymbolTable(tuples: [
+            ("result", Symbol(type: .arithmeticType(.mutableInt(.u8))))
+        ])
+        let input = Match(expr: Expression.Identifier("result"), clauses: [
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))),
+                         block: Block(children: [])),
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.bool(.mutableBool)),
+                         block: Block(children: []))
+        ], elseClause: nil)
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        XCTAssertThrowsError(try compiler.run(input)) {
+            let compilerError = $0 as? CompilerError
+            XCTAssertNotNil(compilerError)
+            XCTAssertEqual(compilerError?.message, "extraneous clause in match statement: bool")
+        }
+    }
+
+    func testCompileMatchStatementWithTwoExtraneousClauses() {
+        let symbols = SymbolTable(tuples: [
+            ("result", Symbol(type: .arithmeticType(.mutableInt(.u8))))
+        ], typeDict: [
+            "None" : .structType(StructType(name: "None", symbols: SymbolTable()))
+        ])
+        let input = Match(expr: Expression.Identifier("result"), clauses: [
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))),
+                         block: Block(children: [])),
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.bool(.mutableBool)),
+                         block: Block(children: [])),
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.Identifier("None"),
+                         block: Block(children: []))
+        ], elseClause: nil)
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        XCTAssertThrowsError(try compiler.run(input)) {
+            let compilerError = $0 as? CompilerError
+            XCTAssertNotNil(compilerError)
+            XCTAssertEqual(compilerError?.message, "extraneous clauses in match statement: bool, None")
+        }
+    }
+
+    func testCompileMatchStatementWithOnlyOneClause() throws {
+        let symbols = SymbolTable(tuples: [
+            ("result", Symbol(type: .arithmeticType(.mutableInt(.u8)))),
+            ("test", Symbol(type: .arithmeticType(.mutableInt(.u8))))
+        ])
+        let input = Match(expr: Expression.Identifier("test"), clauses: [
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))),
+                         block: Block(children: [
+                            Expression.Assignment(lexpr: Expression.Identifier("result"), rexpr: Expression.Identifier("foo"))
+                        ]))
+        ], elseClause: nil)
+        let expected = Block(children: [
+            VarDeclaration(identifier: Expression.Identifier("__index"),
+                           explicitType: nil,
+                           expression: Expression.Identifier("test"),
+                           storage: .automaticStorage,
+                           isMutable: true),
+            If(condition: Expression.Is(expr: Expression.Identifier("__index"),
+                                        testType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8)))), then: Block(children: [
+                VarDeclaration(identifier: Expression.Identifier("foo"),
+                               explicitType: nil,
+                               expression: Expression.As(expr: Expression.Identifier("__index"), targetType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8)))),
+                               storage: .automaticStorage,
+                               isMutable: false),
+                Block(children: [
+                    Expression.Assignment(lexpr: Expression.Identifier("result"),
+                                          rexpr: Expression.Identifier("foo"))
+                ])
+            ])),
+        ])
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        let output = try compiler.run(input)
+        XCTAssertEqual(output, expected)
+    }
+
+    func testCompileMatchStatementWithUnionTypeAndNonexhaustiveClauses() {
+        let symbols = SymbolTable(tuples: [
+            ("result", Symbol(type: .arithmeticType(.mutableInt(.u8)))),
+            ("test", Symbol(type: .unionType(UnionType([.arithmeticType(.mutableInt(.u8)), .bool(.mutableBool)]))))
+        ])
+        let input = Match(expr: Expression.Identifier("test"), clauses: [
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))),
+                         block: Block(children: [
+                            Expression.Assignment(lexpr: Expression.Identifier("result"), rexpr: Expression.LiteralInt(1))
+                        ]))
+        ], elseClause: nil)
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        XCTAssertThrowsError(try compiler.run(input)) {
+            let compilerError = $0 as? CompilerError
+            XCTAssertNotNil(compilerError)
+            XCTAssertEqual(compilerError?.message, "match statement is not exhaustive. Missing clause: bool")
+        }
+    }
+
+    func testCompileMatchStatementWithUnionTypeAndExhaustiveClauses() throws {
+        let symbols = SymbolTable(tuples: [
+            ("result", Symbol(type: .arithmeticType(.mutableInt(.u8)))),
+            ("test", Symbol(type: .unionType(UnionType([.arithmeticType(.mutableInt(.u8)), .bool(.mutableBool)]))))
+        ])
+        let input = Match(expr: Expression.Identifier("test"), clauses: [
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))),
+                         block: Block(children: [
+                            Expression.Assignment(lexpr: Expression.Identifier("result"), rexpr: Expression.LiteralInt(1))
+                        ])),
+            Match.Clause(valueIdentifier: Expression.Identifier("foo"),
+                         valueType: Expression.PrimitiveType(.bool(.mutableBool)),
+                         block: Block(children: [
+                            Expression.Assignment(lexpr: Expression.Identifier("result"), rexpr: Expression.LiteralInt(2))
+                        ]))
+        ], elseClause: nil)
+        let expected = Block(children: [
+            VarDeclaration(identifier: Expression.Identifier("__index"),
+                           explicitType: nil,
+                           expression: Expression.Identifier("test"),
+                           storage: .automaticStorage,
+                           isMutable: true),
+            If(condition: Expression.Is(expr: Expression.Identifier("__index"),
+                                        testType: Expression.PrimitiveType(.bool(.mutableBool))), then: Block(children: [
+                VarDeclaration(identifier: Expression.Identifier("foo"),
+                               explicitType: nil,
+                               expression: Expression.As(expr: Expression.Identifier("__index"), targetType: Expression.PrimitiveType(.bool(.mutableBool))),
+                               storage: .automaticStorage,
+                               isMutable: false),
+                Block(children: [
+                    Expression.Assignment(lexpr: Expression.Identifier("result"),
+                                          rexpr: Expression.LiteralInt(2))
+                ])
+            ]), else: If(condition: Expression.Is(expr: Expression.Identifier("__index"),
+                                                  testType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8)))), then: Block(children: [
+                          VarDeclaration(identifier: Expression.Identifier("foo"),
+                                         explicitType: nil,
+                                         expression: Expression.As(expr: Expression.Identifier("__index"), targetType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8)))),
+                                         storage: .automaticStorage,
+                                         isMutable: false),
+                          Block(children: [
+                              Expression.Assignment(lexpr: Expression.Identifier("result"),
+                                                    rexpr: Expression.LiteralInt(1))
+                          ])
+                      ]))
+            )
+        ])
+        let compiler = CompilerPassMatch(symbols: symbols, globalEnvironment: GlobalEnvironment(memoryLayoutStrategy: MemoryLayoutStrategyTurtleTTL()))
+        let output = try compiler.run(input)
+        XCTAssertEqual(output, expected)
+    }
+}

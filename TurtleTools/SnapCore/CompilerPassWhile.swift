679466c70fe26df897cb71b3cafadd5b9ac3d34c
foxostro <foxostro@gmail.com>
2024-08-26 21:31:40 -0700

    Add CompilerPassWhile

---
+++ TurtleTools/SnapCore/CompilerPassWhile.swift
@@ -0,0 +1,37 @@
+//
+//  CompilerPassWhile.swift
+//  SnapCore
+//
+//  Created by Andrew Fox on 8/26/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import TurtleCore
+
+/// Compiler pass to lower and erase "while" statements
+public class CompilerPassWhile: CompilerPassWithDeclScan {
+    public override func visit(while node0: While) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try SnapSubcompilerWhile().compile(
+            while: node0,
+            symbols: symbols!,
+            labelMaker: globalEnvironment.labelMaker)
+        let node2 = try super.visit(node1)
+        return node2
+    }
+    
+    public override func visit(varDecl node0: VarDeclaration) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try super.visit(varDecl: node0) as! VarDeclaration
+        let subcompiler = SnapSubcompilerVarDeclaration(symbols: symbols!, globalEnvironment: globalEnvironment)
+        let node2 = try subcompiler.compile(node1)
+        return node2
+    }
+}
+
+extension AbstractSyntaxTreeNode {
+    /// Compiler pass to lower and erase "while" statements
+    public func whilePass(_ globalEnvironment: GlobalEnvironment) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try clearSymbols(globalEnvironment)
+        let node2 = try CompilerPassWhile(globalEnvironment: globalEnvironment).run(node1)
+        return node2
+    }
+}

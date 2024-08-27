752b6aebdb100dd294f62b6f3e5a09e5fc0a184a
foxostro <foxostro@gmail.com>
2024-08-26 21:31:38 -0700

    Add CompilerPassIf

---
+++ TurtleTools/SnapCore/CompilerPassIf.swift
@@ -0,0 +1,37 @@
+//
+//  CompilerPassIf.swift
+//  SnapCore
+//
+//  Created by Andrew Fox on 8/26/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import TurtleCore
+
+/// Compiler pass to lower and erase "if" statements
+public class CompilerPassIf: CompilerPassWithDeclScan {
+    public override func visit(if node0: If) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try SnapSubcompilerIf().compile(
+            if: node0,
+            symbols: symbols!,
+            labelMaker: globalEnvironment.labelMaker)
+        let node2 = try super.visit(node1)
+        return node2
+    }
+    
+    public override func visit(varDecl node0: VarDeclaration) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try super.visit(node0) as! VarDeclaration
+        let subcompiler = SnapSubcompilerVarDeclaration(symbols: symbols!, globalEnvironment: globalEnvironment)
+        let node2 = try subcompiler.compile(node1)
+        return node2
+    }
+}
+
+extension AbstractSyntaxTreeNode {
+    /// Compiler pass to lower and erase "if" statements
+    public func ifPass(_ globalEnvironment: GlobalEnvironment) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try clearSymbols(globalEnvironment)
+        let node2 = try CompilerPassIf(globalEnvironment: globalEnvironment).run(node1)
+        return node2
+    }
+}

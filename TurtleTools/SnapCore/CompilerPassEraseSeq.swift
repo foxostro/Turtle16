b61b997cfdd050ab0b9e9027298df3c754324295
foxostro <foxostro@gmail.com>
2024-08-28 09:36:48 -0700

    Add eraseSeq(matching:) to erase all Seq in the AST matching a predicate
    If the Seq matches the predicate then it is erased from the AST.
    This will be useful soon for removing scopePrologue sequences from the
    output of a compiler pass.

---
+++ TurtleTools/SnapCore/CompilerPassEraseSeq.swift
@@ -0,0 +1,29 @@
+//
+//  CompilerPassEraseSeq.swift
+//  SnapCore
+//
+//  Created by Andrew Fox on 8/28/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import TurtleCore
+
+public class CompilerPassEraseSeq: CompilerPass {
+    public typealias Predicate = (Seq) -> Bool // TODO: Consider moving the project minOS to 14+ and switching to Swift's built-in Predicate type
+    let predicate: Predicate
+    
+    public init(matching predicate: @escaping Predicate) {
+        self.predicate = predicate
+    }
+    
+    public override func visit(seq: Seq) throws -> AbstractSyntaxTreeNode? {
+        predicate(seq) ? nil : seq
+    }
+}
+
+extension AbstractSyntaxTreeNode {
+    // Remove all Seqs from the AST which match the given predicate
+    public func eraseSeq(matching predicate: @escaping CompilerPassEraseSeq.Predicate) throws -> AbstractSyntaxTreeNode? {
+        try CompilerPassEraseSeq(matching: predicate).run(self)
+    }
+}

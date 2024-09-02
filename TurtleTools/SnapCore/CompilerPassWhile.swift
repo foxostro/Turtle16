b6ceac01128419adfa14a7b0d534c1a4f66ed434
foxostro <foxostro@gmail.com>
2024-09-02 14:14:46 -0700

    Move call to clearSymbols() into CompilerPassWhile

--- TurtleTools/SnapCore/CompilerPassWhile.swift
+++ TurtleTools/SnapCore/CompilerPassWhile.swift
@@ -10,6 +10,12 @@ import TurtleCore
 
 /// Compiler pass to lower and erase "while" statements
 public class CompilerPassWhile: CompilerPassWithDeclScan {
+    public override func run(_ node0: AbstractSyntaxTreeNode?) throws -> AbstractSyntaxTreeNode? {
+        let node1 = try node0?.clearSymbols(globalEnvironment)
+        let node2 = try super.run(node1)
+        return node2
+    }
+    
     public override func visit(while node0: While) throws -> AbstractSyntaxTreeNode? {
         let node1 = try SnapSubcompilerWhile().compile(
             while: node0,
@@ -23,8 +29,6 @@ public class CompilerPassWhile: CompilerPassWithDeclScan {
 extension AbstractSyntaxTreeNode {
     /// Compiler pass to lower and erase "while" statements
     public func whilePass(_ globalEnvironment: GlobalEnvironment) throws -> AbstractSyntaxTreeNode? {
-        let node1 = try clearSymbols(globalEnvironment)
-        let node2 = try CompilerPassWhile(globalEnvironment: globalEnvironment).run(node1)
-        return node2
+        try CompilerPassWhile(globalEnvironment: globalEnvironment).run(self)
     }
 }

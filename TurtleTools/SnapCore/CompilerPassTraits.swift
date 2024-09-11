537cd3fbbe89ed1dba3066921429a3a5825ba1fb
foxostro <foxostro@gmail.com>
2024-09-11 09:15:55 -0700

    CompilerPassTraits: produces vtables and trait objects for each trait
    For each trait declaration, this compiler pass produces corresponding
    vtable types, a vtable instance, and trait object type.

--- TurtleTools/SnapCore/CompilerPassTraits.swift
+++ TurtleTools/SnapCore/CompilerPassTraits.swift
@@ -12,13 +12,38 @@ import TurtleCore
 // TODO: This compiler pass ought also rewrite expressions to erase traits completely
 public class CompilerPassTraits: CompilerPassWithDeclScan {
     override func scan(trait node: TraitDeclaration) throws {
-        try super.scan(trait: node)
+        // TODO: remove the scan(trait:) override when we change the super class to replace SnapSubcompilerTraitDeclaration with TraitScanner
+        let scanner = TraitScanner(globalEnvironment: globalEnvironment, symbols: symbols!)
+        try scanner.scan(trait: node)
     }
     
-    public override func visit(trait node0: TraitDeclaration) throws -> AbstractSyntaxTreeNode? {
-        assert(!node0.isGeneric)
-        let node1 = try super.visit(trait: node0)
-        return node1
+    public override func visit(trait traitDecl0: TraitDeclaration) throws -> AbstractSyntaxTreeNode? {
+        assert(!traitDecl0.isGeneric)
+        let traitDecl1 = try super.visit(trait: traitDecl0) as! TraitDeclaration
+        let traitType = try symbols!.resolveType(identifier: traitDecl1.identifier.identifier).unwrapTraitType()
+        let vtableDecl = traitType
+            .vtableStructDeclaration
+            .withSourceAnchor(traitDecl1.sourceAnchor)
+            .withVisibility(traitDecl1.visibility)
+        let voidPtr = Expression.PointerType(Expression.PrimitiveType(.void))
+        let vtableType = Expression.PointerType(Expression.ConstType(Expression.Identifier(traitDecl1.nameOfVtableType)))
+        let traitObjectDecl = StructDeclaration(
+            sourceAnchor: traitDecl1.sourceAnchor,
+            identifier: Expression.Identifier(
+                sourceAnchor: traitDecl1.identifier.sourceAnchor,
+                identifier: traitDecl1.nameOfTraitObjectType),
+            members: [
+                StructDeclaration.Member(name: "object", type: voidPtr),
+                StructDeclaration.Member(name: "vtable", type: vtableType)
+            ],
+            visibility: traitDecl1.visibility,
+            isConst: true)
+        let seq = Seq(children: [
+            traitDecl1,
+            vtableDecl,
+            traitObjectDecl
+        ])
+        return seq
     }
 }
 
--- TurtleTools/SnapCoreTests/CompilerPassTraitsTests.swift
+++ TurtleTools/SnapCoreTests/CompilerPassTraitsTests.swift
@@ -12,7 +12,112 @@ import SnapCore
 
 final class CompilerPassTraitsTests: XCTestCase {
 
-    func testExample() throws {
+    func testEmptyTrait() throws {
+        let traitIdent = Expression.Identifier("Foo")
+        let traitObjectIdent = Expression.Identifier("__Foo_object")
+        let vtableIdent = Expression.Identifier("__Foo_vtable")
+        let vtableType = Expression.PointerType(Expression.ConstType(vtableIdent))
+        let objectType = Expression.PointerType(Expression.PrimitiveType(.void))
+        
+        let expected = Block(children: [
+            Seq(children: [
+                TraitDeclaration(
+                    identifier: traitIdent,
+                    members: []),
+                StructDeclaration(
+                    identifier: vtableIdent,
+                    members: [],
+                    isConst: true),
+                StructDeclaration(
+                    identifier: traitObjectIdent,
+                    members: [
+                        StructDeclaration.Member(
+                            name: "object",
+                            type: objectType),
+                        StructDeclaration.Member(
+                            name: "vtable",
+                            type: vtableType)
+                    ],
+                    isConst: true)
+            ])
+        ])
+        
+        let input = Block(
+            children: [
+                TraitDeclaration(
+                    identifier: traitIdent,
+                    members: [])
+            ],
+            id: expected.id)
+        
+        let actual = try input.traitsPass(GlobalEnvironment())
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testSimpleConcreteTrait() throws {
+        let traitIdent = Expression.Identifier("Serial")
+        let traitObjectIdent = Expression.Identifier("__Serial_object")
+        let vtableIdent = Expression.Identifier("__Serial_vtable")
+        let vtableType = Expression.PointerType(Expression.ConstType(vtableIdent))
+        let objectType = Expression.PointerType(Expression.PrimitiveType(.void))
+        let putsFnType = Expression.PointerType(
+            Expression.FunctionType(
+                name: nil,
+                returnType: Expression.PrimitiveType(.void),
+                arguments: [
+                    Expression.PointerType(Expression.Identifier("Serial")),
+                    Expression.DynamicArrayType(
+                        Expression.PrimitiveType(
+                            .arithmeticType(.mutableInt(.u8))))
+                ]))
+        
+        let expected = Block(children: [
+            Seq(children: [
+                TraitDeclaration(
+                    identifier: traitIdent,
+                    members: [
+                        TraitDeclaration.Member(name: "puts", type:  putsFnType)
+                    ]),
+                StructDeclaration(
+                    identifier: vtableIdent,
+                    members: [
+                        StructDeclaration.Member(
+                            name: "puts",
+                            type: Expression.PointerType(
+                                Expression.FunctionType(
+                                    returnType: Expression.PrimitiveType(.void),
+                                    arguments: [
+                                        Expression.PointerType(Expression.Identifier("Serial")),
+                                        Expression.DynamicArrayType(Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))))
+                                    ])))
+                    ],
+                    isConst: true),
+                StructDeclaration(
+                    identifier: traitObjectIdent,
+                    members: [
+                        StructDeclaration.Member(
+                            name: "object",
+                            type: objectType),
+                        StructDeclaration.Member(
+                            name: "vtable",
+                            type: vtableType)
+                    ],
+                    isConst: true)
+            ])
+        ])
+        
+        let input = Block(
+            children: [
+                TraitDeclaration(
+                    identifier: traitIdent,
+                    members: [
+                        TraitDeclaration.Member(name: "puts", type:  putsFnType)
+                    ])
+            ],
+            id: expected.id)
+        
+        let actual = try input.traitsPass(GlobalEnvironment())
+        XCTAssertEqual(actual, expected)
     }
 
 }

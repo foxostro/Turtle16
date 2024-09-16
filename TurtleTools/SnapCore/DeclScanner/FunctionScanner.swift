d2c7d17379e28b1bafdc67f2bb8960720bc1a3ca
foxostro <foxostro@gmail.com>
2024-09-15 18:05:20 -0700

    Added FunctionScanner

---
+++ TurtleTools/SnapCore/DeclScanner/FunctionScanner.swift
@@ -0,0 +1,112 @@
+//
+//  FunctionScanner.swift
+//  SnapCore
+//
+//  Created by Andrew Fox on 9/15/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import Foundation
+import TurtleCore
+
+/// Scans a function declaration and binds the function symbol in the environment
+public class FunctionScanner: NSObject {
+    public let globalEnvironment: GlobalEnvironment
+    public let symbols: SymbolTable
+    public let enclosingImplId: AbstractSyntaxTreeNode.ID?
+    
+    private var memoryLayoutStrategy: MemoryLayoutStrategy {
+        globalEnvironment.memoryLayoutStrategy
+    }
+    
+    public init(
+        globalEnvironment: GlobalEnvironment = GlobalEnvironment(),
+        symbols: SymbolTable = SymbolTable(),
+        enclosingImplId: AbstractSyntaxTreeNode.ID? = nil) {
+
+        self.globalEnvironment = globalEnvironment
+        self.symbols = symbols
+        self.enclosingImplId = enclosingImplId
+    }
+    
+    public func scan(func node: FunctionDeclaration) throws {
+        assert(node.symbols.frameLookupMode.isSet)
+        let name = node.identifier.identifier
+        
+        guard symbols.existsAndCannotBeShadowed(identifier: name) == false else {
+            throw CompilerError(sourceAnchor: node.identifier.sourceAnchor,
+                                message: "function redefines existing symbol: `\(name)'")
+        }
+        
+        if node.isGeneric {
+            try doGeneric(node: node)
+        }
+        else {
+            try doNonGeneric(node: node)
+        }
+    }
+    
+    private func doGeneric(node: FunctionDeclaration) throws {
+        let name = node.identifier.identifier
+        let typ = Expression.GenericFunctionType(
+            template: node,
+            enclosingImplId: enclosingImplId)
+        let symbol = Symbol(
+            type: .genericFunction(typ),
+            offset: 0,
+            storage: .automaticStorage,
+            visibility: node.visibility)
+        symbols.bind(identifier: name, symbol: symbol)
+    }
+    
+    private func doNonGeneric(node node0: FunctionDeclaration) throws {
+        let functionType = try TypeContextTypeChecker(symbols: symbols)
+            .check(expression: node0.functionType)
+            .unwrapFunctionType()
+        
+        node0.symbols.enclosingFunctionTypeMode = .set(functionType)
+        node0.symbols.enclosingFunctionNameMode = .set(node0.identifier.identifier)
+        node0.body.symbols.parent = node0.symbols
+        
+        var offset = memoryLayoutStrategy.sizeOfSaveArea
+        
+        for i in (0..<functionType.arguments.count).reversed() {
+            let argumentType = functionType.arguments[i]
+            let argumentName = node0.argumentNames[i]
+            let symbol = Symbol(
+                type: argumentType.correspondingConstType,
+                offset: -offset,
+                storage: .automaticStorage)
+            node0.symbols.bind(identifier: argumentName, symbol: symbol)
+            let sizeOfArugmentType = memoryLayoutStrategy.sizeof(type: argumentType)
+            offset += sizeOfArugmentType
+        }
+        
+        // Bind a special symbol to contain the function return value.
+        // This must be located just before the function arguments.
+        let kReturnValueIdentifier = "__returnValue"
+        node0.symbols.bind(
+            identifier: kReturnValueIdentifier,
+            symbol: Symbol(
+                type: functionType.returnType,
+                offset: -offset,
+                storage: .automaticStorage))
+        let sizeOfFunctionReturnType = memoryLayoutStrategy.sizeof(type: functionType.returnType)
+        offset += sizeOfFunctionReturnType
+        
+        let node1 = node0.withFunctionType(Expression.FunctionType(
+            sourceAnchor: node0.functionType.sourceAnchor,
+            name: functionType.name,
+            returnType: Expression.PrimitiveType(functionType.returnType),
+            arguments: functionType.arguments.map(\.lift)))
+        
+        functionType.ast = node1
+        
+        let symbol = Symbol(
+            type: .function(functionType),
+            offset: 0,
+            storage: .automaticStorage,
+            visibility: node1.visibility)
+        symbols.bind(identifier: node1.identifier.identifier, symbol: symbol)
+    }
+}
---
+++ TurtleTools/SnapCoreTests/DeclScanner/FunctionScannerTests.swift
@@ -0,0 +1,86 @@
+//
+//  FunctionScannerTests.swift
+//  SnapCoreTests
+//
+//  Created by Andrew Fox on 9/15/24.
+//  Copyright © 2024 Andrew Fox. All rights reserved.
+//
+
+import XCTest
+import SnapCore
+import TurtleCore
+
+final class FunctionScannerTests: XCTestCase {
+    func testFunctionRedefinesExistingSymbol() throws {
+        let symbols = SymbolTable()
+        symbols.bind(identifier: "foo", symbol: Symbol(type: .void))
+        
+        let input = FunctionDeclaration(
+            identifier: Expression.Identifier("foo"),
+            functionType: Expression.FunctionType(
+                name: "foo",
+                returnType: Expression.PrimitiveType(.arithmeticType(.mutableInt(.u8))),
+                arguments: []),
+            argumentNames: [],
+            body: Block(children: []))
+            .reconnect(parent: nil)
+        
+        let scanner = FunctionScanner(symbols: symbols)
+        XCTAssertThrowsError(try scanner.scan(func: input)) {
+            let error = $0 as? CompilerError
+            XCTAssertEqual(error?.message, "function redefines existing symbol: `foo\'")
+        }
+    }
+    
+    func testDeclareFunction() throws {
+        let input = FunctionDeclaration(
+            identifier: Expression.Identifier("foo"),
+            functionType: Expression.FunctionType(
+                name: "foo",
+                returnType: Expression.PrimitiveType(.void),
+                arguments: []),
+            argumentNames: [],
+            body: Block(children: []))
+            .reconnect(parent: nil)
+        let functionType = FunctionType(
+            name: "foo",
+            mangledName: "foo",
+            returnType: .void,
+            arguments: [],
+            ast: input)
+        let expected = Symbol(
+            type: .function(functionType),
+            offset: 0,
+            storage: .automaticStorage)
+        let scanner = FunctionScanner()
+        try scanner.scan(func: input)
+        let actual = try scanner.symbols.resolve(identifier: "foo")
+        XCTAssertEqual(actual, expected)
+    }
+    
+    func testDeclareGenericFunction() throws {
+        let functionType = Expression.FunctionType(
+            name: "foo",
+            returnType: Expression.Identifier("T"),
+            arguments: [Expression.Identifier("T")])
+        let input = FunctionDeclaration(
+            identifier: Expression.Identifier("foo"),
+            functionType: functionType,
+            argumentNames: ["a"],
+            typeArguments: [Expression.GenericTypeArgument(
+                identifier: Expression.Identifier("T"),
+                constraints: [])],
+            body: Block(children: [
+                Return(Expression.Identifier("a"))
+            ]),
+            visibility: .privateVisibility,
+            symbols: SymbolTable())
+            .reconnect(parent: nil)
+        let scanner = FunctionScanner()
+        try scanner.scan(func: input)
+        let actualSymbol = try scanner.symbols.resolve(identifier: "foo")
+        let actualType = actualSymbol.type
+        let expectedType = SymbolType.genericFunction(Expression.GenericFunctionType(template: input))
+        XCTAssertEqual(actualType, expectedType)
+    }
+}

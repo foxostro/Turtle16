0103922ef9afeb2befc02509fe9793a0e2fac88f
foxostro <foxostro@gmail.com>
2025-01-19 20:47:19 -0800

    Remove SnapCompilerBackEndTurtle16, folding into SnapToTurtle16Compiler

--- TurtleTools/SnapCore/SnapToTurtle16Compiler.swift
+++ TurtleTools/SnapCore/SnapToTurtle16Compiler.swift
@@ -7,6 +7,16 @@
 //
 
 import TurtleCore
+import TurtleSimulatorCore
+
+public struct TurtleProgram {
+    public let testNames: [String]
+    public let symbolsOfTopLevelScope: SymbolTable
+    public let syntaxTree: AbstractSyntaxTreeNode
+    public let tackProgram: TackProgram
+    public let assembly: TopLevel
+    public let instructions: [UInt16]
+}
 
 /// Compile a Snap program to Turtle16 machine code
 public class SnapToTurtle16Compiler: NSObject {
@@ -49,11 +59,56 @@ public class SnapToTurtle16Compiler: NSObject {
     }
 }
 
-public struct TurtleProgram {
-    public let testNames: [String]
-    public let symbolsOfTopLevelScope: SymbolTable
-    public let syntaxTree: AbstractSyntaxTreeNode
-    public let tackProgram: TackProgram
-    public let assembly: TopLevel
-    public let instructions: [UInt16]
+fileprivate extension TackProgram {
+    func machineCode() throws -> ([UInt16], TopLevel) {
+        var assembly: TopLevel!
+        let instructions = try self
+            .assemble()
+            .registerAllocation()
+            .map { assembly = $0; return $0 }
+            .lowerAssembly()
+            .machineCode()
+        return (instructions, assembly)
+    }
+    
+    func assemble() throws -> TopLevel {
+        try TackToTurtle16Compiler().visit(TopLevel(children: [ ast ])) as! TopLevel
+    }
+}
+
+fileprivate extension TopLevel {
+    func map(_ block: (TopLevel) -> TopLevel) -> TopLevel {
+        block(self)
+    }
+    
+    func registerAllocation() throws -> TopLevel {
+        try RegisterAllocatorDriver().compile(topLevel: self)
+    }
+    
+    func lowerAssembly() throws -> TopLevel {
+        let topLevel0 = try SnapSubcompilerSubroutine().visit(self) as! TopLevel
+        
+        // The hardware requires us to place a NOP at the first instruction.
+        let topLevel1: TopLevel = if topLevel0.children.first != InstructionNode(instruction: kNOP) {
+            topLevel0.inserting(
+                children: [
+                    InstructionNode(instruction: kNOP)
+                ],
+                at: 0)
+        }
+        else {
+            topLevel0
+        }
+        
+        return topLevel1
+    }
+    
+    func machineCode() throws -> [UInt16] {
+        let compiler = AssemblerCompiler()
+        compiler.compile(self)
+        if let error = compiler.errors.first {
+            throw error
+        }
+        return compiler.instructions
+    }
 }

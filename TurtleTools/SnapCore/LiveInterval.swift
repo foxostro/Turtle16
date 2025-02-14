ba32020e0e8269269c603015cbf56b5a0ee6629c
foxostro <foxostro@gmail.com>
2025-02-13 20:21:08 -0800

    LiveInterval: style and comment

--- TurtleTools/SnapCore/LiveInterval.swift
+++ TurtleTools/SnapCore/LiveInterval.swift
@@ -6,13 +6,19 @@
 //  Copyright © 2021 Andrew Fox. All rights reserved.
 //
 
+/// Represents a register's live interval for register allocation purposes
 public struct LiveInterval: Equatable {
     public let range: Range<Int>
     public let virtualRegisterName: String
     public let physicalRegisterName: String?
     public let spillSlot: Int?
 
-    public init(range: Range<Int>, virtualRegisterName: String, physicalRegisterName: String?, spillSlot: Int? = nil) {
+    public init(
+        range: Range<Int>,
+        virtualRegisterName: String,
+        physicalRegisterName: String?,
+        spillSlot: Int? = nil
+    ) {
         self.range = range
         self.virtualRegisterName = virtualRegisterName
         self.physicalRegisterName = physicalRegisterName
